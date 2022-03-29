30f51eab47bd0762645f1f1bf1c37b1e72923b9e
Mike Kanning <mkanning@upenn.edu>
2022-03-29 17:25:39 -0400

    add basic specs for User validations. simplify developer login to solicit only email.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'requires an email' do
+    user = described_class.new email: nil
+    expect(user.valid?).to be false
+    expect(user.errors['email']).to include "can't be blank"
+  end
+
+  it 'requires a unique email' do
+    described_class.create email: 'test@upenn.edu'
+    user = described_class.new email: 'test@upenn.edu'
+    expect(user.valid?).to be false
+    expect(user.errors['email']).to include 'has already been taken'
+  end
+
+  it 'requires a unique set of omniauth fields' do
+    described_class.create email: 'test@upenn.edu', provider: 'ldap', uid: 'test'
+    described_class.create email: 'another@upenn.edu', provider: 'saml', uid: 'test'
+    user = described_class.new email: 'more@upenn.edu', provider: 'ldap', uid: 'test'
+    expect(user.valid?).to be false
+    expect(user.errors['uid']).to include 'has already been taken'
+  end
 end
