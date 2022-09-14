44184cea444e80d8aaaf2d236173fa3c50adb55e
Mike Kanning <mkanning@upenn.edu>
2022-09-14 13:07:52 -0400

    add header to AssetCard component. adjust layout and remove some dl fields. show annotation information rather than providing a link.

--- app/components/asset_card/component.rb
+++ app/components/asset_card/component.rb
@@ -3,13 +3,20 @@
 module AssetCard
   # ViewComponent
   class Component < ViewComponent::Base
-    attr_reader :asset
+    attr_reader :asset, :index
 
     # @param [AssetResource] asset
     # @param [Integer] index
-    def initialize(asset:, index: nil)
+    # @param [TrueClass, FalseClass] arranged
+    def initialize(asset:, arranged: false, index: nil)
       @asset = asset
       @index = index ? index + 1 : nil
+      @arranged = arranged
+    end
+
+    # @return [String]
+    def card_title
+      [index, asset.original_filename, asset.label].compact.join(' - ')
     end
 
     # @return [Array<AssetResource::Annotation>]
@@ -34,6 +41,21 @@ module AssetCard
       link_to('Download Access Copy',
               file_asset_path(@asset, type: :access, disposition: 'attachment'),
               class: 'stretched-link')
+    end
+
+    # @return [String]
+    def size
+      number_to_human_size asset.technical_metadata.size
+    end
+
+    # @return [String]
+    def mime_type
+      asset.technical_metadata.mime_type
+    end
+
+    # @return [TrueClass, FalseClass]
+    def arranged?
+      @arranged
     end
   end
 end
