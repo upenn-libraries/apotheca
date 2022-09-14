1a680db153f669dc7c4779956b8ad1b9eb2558cc
Mike Kanning <mkanning@upenn.edu>
2022-09-14 13:33:42 -0400

    add ItemResource asset_count method for use across multiple view partials

--- app/resources/item_resource.rb
+++ app/resources/item_resource.rb
@@ -37,6 +37,12 @@ class ItemResource < Valkyrie::Resource
   # Asset IDs
   attribute :asset_ids, Valkyrie::Types::Array.of(Valkyrie::Types::ID).optional
 
+
+  # @return [Integer]
+  def asset_count
+    asset_ids.length
+  end
+
   # @return [Array<Valkyrie::ID>]
   def unarranged_asset_ids
     asset_ids - structural_metadata.arranged_asset_ids
