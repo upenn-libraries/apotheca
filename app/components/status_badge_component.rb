a05df0c24b011616ae8e7fea7dbdbbe49cf32032
Mike Kanning <mkanning@upenn.edu>
2022-09-22 16:03:33 -0400

    change PreservationStatusComponent into a more generic StatusBadgeComponent. use new component to render badges for expected derivatives rather than DerivativeInventoryComponent.

---
+++ app/components/status_badge_component.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Render a badge with style and label
+class StatusBadgeComponent < ViewComponent::Base
+  attr_reader :test, :truthy, :falsey
+
+  # @param [TrueClass, FalseClass] test
+  # @param [String] truthy
+  # @param [String] falsey
+  def initialize(test:, truthy:, falsey:)
+    @test = test
+    @truthy = truthy
+    @falsey = falsey
+  end
+
+  # @return [Array<String (frozen)>]
+  def classes
+    test ? ['bg-success'] : %w[bg-warning text-dark]
+  end
+
+  # @return [String (frozen)]
+  def label
+    test ? truthy : falsey
+  end
+
+  # @return [ActiveSupport::SafeBuffer]
+  def call
+    content_tag :span, label, class: classes.push('badge')
+  end
+end
