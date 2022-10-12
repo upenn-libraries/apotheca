b7df7e303a63a576def6121dd89f16ff0a2ba1a7
Mike Kanning <mkanning@upenn.edu>
2022-10-12 10:17:13 -0400

    add note and consistent semicolon use in TabsController JS

--- app/components/tabs/tabs_controller.js
+++ app/components/tabs/tabs_controller.js
@@ -5,8 +5,9 @@ export default class extends Controller {
         const anchor = window.location.hash;
         const elem = document.querySelector(anchor + '-tab');
         if(elem) {
+            // init Bootstrap Tab object - https://getbootstrap.com/docs/5.0/components/navs-tabs/#show
             const tab = new bootstrap.Tab(anchor + '-tab');
-            tab.show()
+            tab.show();
         }
     }
     updateUrl(event) {
