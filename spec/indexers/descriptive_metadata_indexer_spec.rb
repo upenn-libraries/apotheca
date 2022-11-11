bf61a4a89035248ea38101a014347603a8413f42
Mike Kanning <mkanning@upenn.edu>
2022-11-11 15:35:50 -0500

    add spec to DescriptiveMetadataIndexerSpec that check for proper setting of values from data sources

--- spec/indexers/descriptive_metadata_indexer_spec.rb
+++ spec/indexers/descriptive_metadata_indexer_spec.rb
@@ -8,10 +8,14 @@ RSpec.describe DescriptiveMetadataIndexer do
   context 'when an item does not have a bibnumber' do
     let(:resource) { persist(:item_resource) }
 
-    it 'has solr fields for all descriptive metadata fields except bibnumber' do
+    it 'has solr fields for all descriptive metadata fields' do
       ItemResource::DescriptiveMetadata::FIELDS.each do |f|
         expect(result.keys).to include(/#{f}/)
       end
+    end
+
+    it 'has values from the Resource' do
+      expect(result[:title_tsi]).to eq resource.descriptive_metadata.title.first
     end
 
     it 'has solr fields with JSON representation of source metadata' do
@@ -25,6 +29,11 @@ RSpec.describe DescriptiveMetadataIndexer do
     # this is taken from spec/services/metadata_extractor/marmite/client_spec.rb
     # TODO: perhaps use a shared context, or a stub/mock of the service injected into the indexer?
     let(:marc_xml) { File.read(file_fixture('marmite/marc_xml/book-1.xml')) }
+    let(:expected_subjects) do
+      ['Metallurgy -- Early works to 1800.',
+       'Assaying -- Early works to 1800.',
+       'PU', 'PU', 'PU']
+    end
 
     before do
       stub_request(:get, 'https://marmite.library.upenn.edu:9292/api/v2/records/123/marc21?update=always')
@@ -32,7 +41,12 @@ RSpec.describe DescriptiveMetadataIndexer do
     end
 
     it 'has field values that prefer values from the Resource' do
-      expect(result[:title_tsi]).to eq 'Test Item'
+      expect(result[:title_tsi]).to eq resource.descriptive_metadata.title.first
+    end
+
+    it 'has values from MARC metadata when Resource fields are blank' do
+      expect(result[:personal_name_tsim]).to eq ['Feyerabend, Johann,']
+      expect(result[:subject_tsim]).to match_array(expected_subjects)
     end
 
     it 'has solr fields with JSON representation of source metadata' do
