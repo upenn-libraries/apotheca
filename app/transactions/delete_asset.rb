d86caa1ff67c51b5d1341de4a2e6727e419e0464
Carla Galarza <cgalarza@upenn.edu>
2022-11-29 11:39:18 -0500

    Deleting derivatives, preservation file and preservation copies when deleting an Asset

--- app/transactions/delete_asset.rb
+++ app/transactions/delete_asset.rb
@@ -7,5 +7,29 @@ class DeleteAsset
   include Dry::Transaction(container: Container)
 
   step :find_asset, with: 'asset_resource.find_resource'
+  step :delete_files
   step :delete_asset, with: 'asset_resource.delete_resource'
+
+  # Deletes preservation and derivative files
+  def delete_files(resource:)
+    if resource.preservation_file_id.present?
+      delete_file(resource.preservation_file_id)
+    end
+
+    (resource.derivatives || []).each do |derivative|
+      delete_file(derivative.file_id)
+    end
+
+    (resource.preservation_copies_ids || []).each do |preservation_copy_id|
+      delete_file(preservation_copy_id)
+    end
+
+    Success(resource: resource)
+  end
+
+  private
+
+  def delete_file(id)
+    Valkyrie::StorageAdapter.delete(id: id)
+  end
 end
