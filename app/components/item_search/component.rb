4c29e11233cfb2089975057fb3ffaebd56a83135
Mike Kanning <mkanning@upenn.edu>
2022-12-01 11:08:06 -0500

    consistently use search_params variable in item search component

--- app/components/item_search/component.rb
+++ app/components/item_search/component.rb
@@ -17,45 +17,45 @@ module ItemSearch
     end
 
     def rows_options
-      options_for_select([50, 100, 250, 500], params[:rows])
+      options_for_select([50, 100, 250, 500], search_params[:rows])
     end
 
     def sort_fields_options
-      options_for_select(mapper::Sort.field_map, params.dig(:sort, :field))
+      options_for_select(mapper::Sort.field_map, search_params.dig(:sort, :field))
     end
 
     def sort_directions_options
       options_for_select(
         [%w[Descending desc], %w[Ascending asc]],
-        params.dig(:sort, :direction)
+        search_params.dig(:sort, :direction)
       )
     end
 
     def published_options
       options_for_select(
         [['Yes', true], ['No', false]],
-        params.dig(:filter, :published)
+        search_params.dig(:filter, :published)
       )
     end
 
     def created_by_options
       options_for_select(
         container.facets.fetch(mapper::Filter.created_by.to_s, []),
-        params.dig(:filter, :created_by)
+        search_params.dig(:filter, :created_by)
       )
     end
 
     def updated_by_options
       options_for_select(
         container.facets.fetch(mapper::Filter.updated_by.to_s, []),
-        params.dig(:filter, :updated_by)
+        search_params.dig(:filter, :updated_by)
       )
     end
 
     def collections_options
       options_for_select(
         container.facets.fetch(mapper::Filter.collection.to_s, []),
-        params.dig(:filter, :collection)
+        search_params.dig(:filter, :collection)
       )
     end
 
