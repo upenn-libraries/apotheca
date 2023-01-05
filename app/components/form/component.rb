afcf62b6973287aac7dfddec623fc4353722798a
Carla Galarza <cgalarza@upenn.edu>
2023-01-05 12:48:19 -0500

    Adding more documentation

--- app/components/form/component.rb
+++ app/components/form/component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Form
-  # Renders a form element. Has slots for inputs, a submit button and an error message. If desired,
+  # Renders a form element. Has slots for fields, a submit button and an error message. If desired,
   # inputs can be grouped in sections.
   class Component < ViewComponent::Base
     renders_many :fields, ->(*field_path, **args, &block) {
@@ -16,13 +16,17 @@ module Form
 
     renders_one :submit, SubmitButton::Component
 
-    # Requires name and url. Any additional arguments will be passed to the form_tag helper.
+    # Requires name and url. If a model is provided the form method can be assumed and field names
+    # and values don't have to be explicitly provided. Any additional arguments will be passed
+    # to the form_tag helper.
     #
-    # Often times a `:method` parameter should be provided. If the form contains file inputs a truthy
-    # `:multipart` parameter should be provided.
+    # A `:method` parameter should be provided if the action is something other than creating
+    # or updating a record. If the form contains file inputs a truthy `:multipart` parameter
+    #  should be provided.
     #
     # @param [String] name given to form, passed to backend to identify form
     # @param [String] url for request
+    # @param [Valkyrie::ChangeSet] model or change set that the form is representing
     # @param [Hash] options (see ActionView::Helpers::FormTagHelper.form_tag)
     def initialize(name:, url:, model: nil, **options)
       @name = name
@@ -35,6 +39,8 @@ module Form
     end
 
     def new_record?
+      raise ArgumentError, 'model must be provided to form to automatically generate url or method' unless @model
+
       if @model.is_a? Valkyrie::ChangeSet
         @model.resource.new_record
       end
