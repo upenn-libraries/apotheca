e6fc40d21686af317821f7b8215758f609f97ec8
mamrey <mamrey@upenn.edu>
2023-01-10 14:00:22 -0500

    Add render right link boolean to header component

--- app/components/header/component.rb
+++ app/components/header/component.rb
@@ -1,15 +1,25 @@
 # frozen_string_literal: true
 
 module Header
+
+
+
+
   class Component < ViewComponent::Base
+
+    attr_reader :render_right_link
+    def initialize(render_right_link: true)
+      @render_right_link = render_right_link
+    end
+
     renders_one :title, ->(tag: :h1, **options, &block) do
       BaseComponent.new(tag, **options, &block)
     end
 
     renders_one :right_link, ->(href:, **options, &block) do
       options[:class] = Array.wrap(options[:class]).append('btn', 'btn-outline-primary')
-
-      BaseComponent.new(:a, href: href, **options, &block)
+        BaseComponent.new(:a, href: href, **options, &block)
     end
+
   end
 end
