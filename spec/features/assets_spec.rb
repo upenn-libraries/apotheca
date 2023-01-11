d5c679c845ae400d0c64bd3ecadda3d0e07c9232
mamrey <mamrey@upenn.edu>
2023-01-11 11:32:49 -0500

    Add tests for viewing asset and thumbnail edit buttons/links within assets tab

--- spec/features/assets_spec.rb
+++ spec/features/assets_spec.rb
@@ -7,12 +7,20 @@ describe 'Asset Management' do
 
   context 'with an admin' do
     let(:user) { create(:user, :admin) }
-    let(:item) { persist(:item_resource) }
+    let(:item) { persist(:item_resource, :with_assets_some_arranged) }
 
     before { visit "#{items_path}/#{item.id}#assets" }
 
     scenario 'viewing link to add and asset' do
       expect(page).to have_link('Add Asset', href: "#{new_asset_path}?item_id=#{item.id}")
+    end
+
+    scenario 'viewing input to edit asset thumbnail' do
+      expect(page).to have_button(value: 'Set as Item Thumbnail', count: item.asset_ids.length)
+    end
+
+    scenario 'viewing links to edit asset' do
+      expect(page).to have_link('Edit Asset', count: item.asset_ids.length)
     end
   end
 
@@ -25,6 +33,14 @@ describe 'Asset Management' do
     scenario 'viewing link to add and asset' do
       expect(page).not_to have_link('Add Asset', href: "#{new_asset_path}?item_id=#{item.id}")
     end
-  end
 
+    scenario 'viewing input to edit asset thumbnail' do
+      expect(page).not_to have_button(value: 'Set as Item Thumbnail')
+    end
+
+    scenario 'viewing links to edit asset' do
+      expect(page).not_to have_link('Edit Asset')
+    end
+
+  end
 end
