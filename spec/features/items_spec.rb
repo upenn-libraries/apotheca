5a550431f1ae4bded0829c8d62da9798b46e57af
mamrey <mamrey@upenn.edu>
2023-01-11 16:55:33 -0500

    Add contexts to scope views in tests

--- spec/features/items_spec.rb
+++ spec/features/items_spec.rb
@@ -6,66 +6,83 @@ describe 'Item management' do
   before { sign_in user }
 
   context 'with an admin' do
-      let(:user) { create(:user, :admin) }
-      let!(:item) { persist(:item_resource) }
+    let(:user) { create(:user, :admin) }
+    let!(:item) { persist(:item_resource, :with_asset) }
 
-      scenario 'viewing create item link' do
-        visit items_path
+    context 'when viewing item index page' do
+      before { visit items_path }
+
+      it 'shows link to create item' do
         expect(page).to have_link('Create Item', href: new_item_path)
       end
+    end
 
-      scenario 'viewing edit item link on descriptive metadata tab' do
-        visit "#{items_path}/#{item.id}"
+    context 'when viewing item show page' do
+      before { visit "#{items_path}/#{item.id}" }
+
+      it 'shows link to edit item on descriptive metadata tab' do
         expect(page).to have_link('Edit', href: "#{items_path}/#{item.id}/edit#descriptive")
       end
 
-      scenario 'viewing edit item link on structural metadata tab' do
-        visit "#{items_path}/#{item.id}"
+      it 'shows link to edit item on structural metadata tab' do
         expect(page).to have_link('Edit', href: "#{items_path}/#{item.id}/edit#structural")
       end
 
-      scenario 'viewing edit item link on administrative info tab' do
-        visit "#{items_path}/#{item.id}"
+      it 'shows link to edit item on administrative info tab' do
         expect(page).to have_link('Edit', href: "#{items_path}/#{item.id}/edit#administrative-info")
+      end
+
+      it 'shows link to edit item thumbnail on assets tab' do
+        expect(page).to have_button(value: 'Set as Item Thumbnail', count: item.asset_ids.length)
       end
     end
 
+  end
 
   context 'with a viewer' do
     let(:user) { create(:user, :viewer) }
-    let!(:item) { persist(:item_resource) }
+    let!(:item) { persist(:item_resource, :with_asset) }
 
-    before { visit items_path }
+    context 'when viewing item index page' do
 
-    it 'lists Item human readable name' do
-      expect(page).to have_link item.human_readable_name, href: item_path(item)
-    end
+      before { visit items_path }
 
-    it 'lists all Item title values' do
+      it 'lists Item human readable name' do
+        expect(page).to have_link item.human_readable_name, href: item_path(item)
+      end
+
+      it 'lists all Item title values' do
         titles = item.descriptive_metadata.title
         titles.each do |title|
           expect(page).to have_selector 'tr li', text: title
         end
       end
 
-    scenario 'viewing create item link' do
-      visit items_path
-      expect(page).not_to have_link('Create Item', href: new_item_path)
+      it 'does not show link to create item' do
+        visit items_path
+        expect(page).not_to have_link('Create Item', href: new_item_path)
+      end
     end
 
-    scenario 'viewing edit item link on descriptive metadata tab' do
-      visit "#{items_path}/#{item.id}"
-      expect(page).not_to have_link('Edit', href: "#{items_path}/#{item.id}/edit#descriptive")
+    context 'when viewing item show page' do
+      before { visit "#{items_path}/#{item.id}" }
+
+      it 'does not show link to edit item on descriptive metadata tab' do
+        expect(page).not_to have_link('Edit', href: "#{items_path}/#{item.id}/edit#descriptive")
+      end
+
+      it 'does not show link to edit item on structural metadata tab' do
+        expect(page).not_to have_link('Edit', href: "#{items_path}/#{item.id}/edit#structural")
+      end
+
+      it 'does not show link to edit item on administrative info tab' do
+        expect(page).not_to have_link('Edit', href: "#{items_path}/#{item.id}/edit#administrative-info")
+      end
+
+      it 'does not show button to set item thumbnail on assets tab' do
+        expect(page).not_to have_button(value: 'Set as Item Thumbnail')
+      end
     end
 
-    scenario 'viewing edit item link on structural metadata tab' do
-      visit "#{items_path}/#{item.id}"
-      expect(page).not_to have_link('Edit', href: "#{items_path}/#{item.id}/edit#structural")
-    end
-
-    scenario 'viewing edit item link on administrative info tab' do
-      visit "#{items_path}/#{item.id}"
-      expect(page).not_to have_link('Edit', href: "#{items_path}/#{item.id}/edit#administrative-info")
-    end
   end
 end
