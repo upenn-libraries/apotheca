f61b88887187244d97b4a2f82d587aa0841f3e7b
Patrick Perkins <lilblizzard97@gmail.com>
2023-01-12 11:37:14 -0500

    change double quotes to single quotes where necessary

--- spec/models/bulk_export_spec.rb
+++ spec/models/bulk_export_spec.rb
@@ -5,23 +5,22 @@ require_relative 'concerns/queueable'
 describe BulkExport do
   it_behaves_like 'queueable'
 
-  it "requires a user" do
+  it 'requires a user' do
     bulk_export = build :bulk_export, user: nil
     expect(bulk_export.valid?).to be false
-    expect(bulk_export.errors['user']).to include "must exist"
+    expect(bulk_export.errors['user']).to include 'must exist'
   end
 
-  it "requires solr params" do
+  it 'requires solr params' do
     bulk_export = build :bulk_export, solr_params: nil
     expect(bulk_export.valid?).to be false
     expect(bulk_export.errors['solr_params']).to include "can't be blank"
   end
 
-  it "requires state to be set" do
+  it 'requires state to be set' do
     bulk_export = build :bulk_export, state: nil
     expect(bulk_export.valid?).to be false
     expect(bulk_export.errors['state']).to include "can't be blank"
   end
-
 end
 
