8752cc874336e6dd3673b8e84ea196a7af853424
Mike Kanning <mkanning@upenn.edu>
2023-01-12 19:07:01 -0500

    avoid passing boolean arranged value into the AssetArrange component at view time

--- app/components/asset_arrange/component.rb
+++ app/components/asset_arrange/component.rb
@@ -5,8 +5,8 @@ module AssetArrange
   class Component < ViewComponent::Base
     attr_reader :item
 
-    renders_one :unarranged_section, Section::Component
-    renders_one :arranged_section, Section::Component
+    renders_one :unarranged_section, ->(**options) { Section::Component.new(arranged: false, **options) }
+    renders_one :arranged_section, ->(**options) { Section::Component.new(arranged: true, **options) }
 
     # @param [ItemResource] item
     def initialize(item:)
