694be78f64ea5ce47a94aac0a995f795b76d37c6
Patrick Perkins <lilblizzard97@gmail.com>
2023-01-17 12:50:54 -0500

    use AASM constant for factory state

--- spec/factories/bulk_export.rb
+++ spec/factories/bulk_export.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :bulk_export do
     association :user, :admin
     solr_params { { search: { all: 'Crunchy' } } }
-    state { 'successful' }
+    state { BulkExport::STATE_SUCCESSFUL }
   end
 end
 
