51fe57afa0aa7a8b624b878d6f912135c5e11d59
Mike Kanning <mkanning@upenn.edu>
2023-01-19 10:58:22 -0500

    simplify item deletion by enqueueing one job for each asset to delete

---
+++ app/jobs/remove_asset_job.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Job to remove an asset. Used when deleting an Item.
+class RemoveAssetJob < ApplicationJob
+  # Convert Asset ID to a Valkyrie::ID object and then use that ID to delete the asset
+  # @param [String] asset_id
+  def perform(asset_id)
+    DeleteAsset.new.call(id: Valkyrie::ID.new(asset_id))
+  end
+end
--- app/transactions/delete_item.rb
+++ app/transactions/delete_item.rb
@@ -6,15 +6,17 @@ class DeleteItem
 
   step :find_item, with: 'item_resource.find_resource'
   step :delete_item, with: 'item_resource.delete_resource'
-  tee :enqueue_job_to_delete_assets
+  tee :delete_assets
 
-  # An Item can have lots of Assets, so we enqueue a job to delete them here.
+  # An Item can have lots of Assets, so we enqueue jobs to delete each of them here.
   # Note that ActiveJob cannot receive an array of Valkyrie::ID objects, so we convert them to strings here.
   # @param [ItemResource] resource
   # @param [TrueClass, FalseClass] async
-  def enqueue_job_to_delete_assets(resource:, async: true)
+  def delete_assets(resource:, async: true)
     asset_ids = resource.asset_ids.map(&:to_s)
     method = async ? 'perform_later' : 'perform_now'
-    RemoveAllAssetsJob.send(method, asset_ids)
+    asset_ids.each do |asset_id|
+      RemoveAssetJob.send(method, asset_id)
+    end
   end
 end
