30ce43ae6c82b1deec6108480d076244099a9cf4
Patrick Perkins <lilblizzard97@gmail.com>
2023-01-25 10:12:47 -0500

    clean up video generator, remove duplicate audio generator code

--- app/services/derivative_service/generator/video.rb
+++ app/services/derivative_service/generator/video.rb
@@ -6,7 +6,7 @@ require 'open3'
 
 module DerivativeService
   module Generator
-    # Generator class encapsulating audio derivative generation logic.
+    # Generator class encapsulating video derivative generation logic.
     class Video < Base
       VALID_MIME_TYPES = %w[video/quicktime].freeze
 
@@ -30,39 +30,23 @@ module DerivativeService
 
     # wrap up ffmpeg interaction
     class FfmpegWrapper
-      # see: https://ffmpeg.org/ffmpeg.html#toc-Generic-options
-      # and: https://trac.ffmpeg.org/wiki/Encode/MP3
-      MP3_OPTIONS = [
-        '-y', # automatically overwrite any existing output files
-        '-qscale:a 5', # quality scale, 0 to 10 - this is roughly 128kbps VBR
-        '-map_metadata -1', # strip any metadata
-        '-ac 2', # ensure 2-channel (stereo) sound
-        '-hide_banner' # hide banner about config/formats from output - remove if debugging
-      ].freeze
-      FFMPEG_EXECUTABLE = 'ffmpeg'
-
       MOV_OPTIONS = [
         '-y',
         '-vcodec h264', # video codec
         '-acodec mp2', # audio codec
       ].freeze
+      FFMPEG_EXECUTABLE = 'ffmpeg'
 
       # @param [String] input_file_path
       # @param [String] output_file_path
-      def self.wav_to_mp3(input_file_path:, output_file_path:)
-        _stdout, stderr, status = Open3.capture3(
-          "#{FFMPEG_EXECUTABLE} -i #{input_file_path} #{MP3_OPTIONS.join(' ')} #{output_file_path}"
-        )
-        raise "FFMpeg Error: #{stderr}" unless status.success?
-
-        true
-      end
-
       def self.mov_to_mp4(input_file_path:, output_file_path:)
         # ffmpeg -i my-video.mov -vcodec h264 -acodec mp2 my-video.mp4
         _stdout, stderr, status = Open3.capture3(
           "#{FFMPEG_EXECUTABLE} -i #{input_file_path} #{MOV_OPTIONS.join(' ')} #{output_file_path}"
         )
+        raise "FFMpeg Error: #{stderr}" unless status.success?
+
+        true
       end
     end
   end
