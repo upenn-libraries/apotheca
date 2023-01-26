dbc6cda5045cfcd5cf68e1e3a8bee60c5d4b11d9
mamrey <mamrey@upenn.edu>
2023-01-26 09:54:17 -0500

    Generate migration to update bulk_exports table

---
+++ db/migrate/20230124222438_add_column_to_bulk_exports.rb
@@ -0,0 +1,7 @@
+class AddColumnToBulkExports < ActiveRecord::Migration[7.0]
+  def change
+    add_column :bulk_exports, :title, :string
+    add_column :bulk_exports, :generated_at, :datetime
+    add_column :bulk_exports, :include_assets, :boolean, default: false
+  end
+end
