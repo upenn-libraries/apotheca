463ed309e50afcb06b8f0315cab24276c6eab4f2
Alyssa Hogan <abhogan@upenn.edu>
2023-02-01 16:42:10 -0500

    Add tests for filtering of Users list

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < ApplicationRecord
   scope :inactive, -> { where(active: false) }
   scope :active_filter, ->(query) { where(active: query) }
   scope :roles_filter, ->(query) { where('? = ANY (roles)', query) }
-  scope :users_search, ->(query) { where('email || first_name || last_name ILIKE ?', "%#{query}%") }
+  scope :users_search, ->(query) { where("email || ' ' || first_name || ' ' || last_name ILIKE ?", "%#{query}%") }
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid, active: true).first_or_create do |user|
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -18,4 +18,57 @@ describe 'User management' do
       expect(page).to have_text 'User created'
     end
   end
+
+  context 'with User searching and filtering' do
+    let(:user) { create(:user, :admin, first_name: 'Carla', last_name: 'Galarza', email: 'cgalarza@upenn.edu') }
+
+    before do
+      @user2 = create(:user, :admin, first_name: 'Michael', last_name: 'Kanning', email: 'mk@upenn.edu')
+      @user3 = create(:user, :editor, first_name: 'Bob', last_name: 'Smith', email: 'bsmith@upenn.edu')
+      @user4 = create(:user, :viewer, first_name: 'Jane', last_name: 'Doe', email: 'jdoe@upenn.edu', active: false)
+
+      visit users_path
+    end
+
+    it 'searches by first name' do
+      fill_in 'Search', with: 'rl'
+      click_on 'Filter'
+      expect(page).to have_selector 'tr.user', count: 1
+      expect(page).to have_text(user.email)
+    end
+
+    it 'searches by last name' do
+      fill_in 'Search', with: '@upenn'
+      click_on 'Filter'
+      expect(page).to have_selector 'tr.user', count: 4
+    end
+
+    it 'searches by email' do
+      fill_in 'Search', with: 'mk'
+      click_on 'Filter'
+      expect(page).to have_selector 'tr.user', count: 1
+      expect(page).to have_text(@user2.email)
+    end
+
+    it 'searches by full name' do
+      fill_in 'Search', with: 'Bob Smith'
+      click_on 'Filter'
+      expect(page).to have_selector 'tr.user', count: 1
+      expect(page).to have_text(@user3.email)
+    end
+
+    it 'filters by status' do
+      select 'Active', from: 'Status'
+      click_on 'Filter'
+      expect(page).to have_selector 'tr.user', count: 3
+      expect(page).not_to have_text @user4.email
+    end
+
+    it 'filters by role' do
+      select 'Viewer', from: 'Role'
+      click_on 'Filter'
+      expect(page).to have_selector 'tr.user', count: 1
+      expect(page).to have_text @user4.email
+    end
+  end
 end
