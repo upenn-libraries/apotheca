1fd9309079089b75ec7124a486faa797b87093dc
Amrey Mathurin <mamrey@upenn.edu>
2023-02-03 13:45:04 -0500

    Add tests for BulkExport authorization

--- spec/models/abilitiy_spec.rb
+++ spec/models/abilitiy_spec.rb
@@ -11,21 +11,27 @@ describe 'Ability' do
     it { is_expected.not_to be_able_to(:view, :any) }
     it { is_expected.not_to be_able_to(:view, ItemResource) }
     it { is_expected.not_to be_able_to(:view, AssetResource) }
+    it { is_expected.not_to be_able_to(:view, BulkExport) }
     it { is_expected.not_to be_able_to(:view, User) }
   end
 
   context 'with a viewer user' do
     let(:user) { create :user, :viewer }
 
+    it { is_expected.to be_able_to(:read, BulkExport)}
     it { is_expected.to be_able_to(:read, ItemResource) }
     it { is_expected.to be_able_to(:read, AssetResource) }
     it { is_expected.not_to be_able_to(:edit, AssetResource) }
     it { is_expected.not_to be_able_to(:edit, ItemResource) }
+    it { is_expected.not_to be_able_to(:create, BulkExport) }
+    it { is_expected.not_to be_able_to(:update, BulkExport) }
+    it { is_expected.not_to be_able_to(:destroy, BulkExport) }
     it { is_expected.not_to be_able_to(:view, User) }
   end
 
   context 'with an editor user' do
     let(:user) { create :user, :editor }
+    let(:bulk_export) { create :bulk_export }
 
     it { is_expected.to be_able_to(:read, AssetResource) }
     it { is_expected.to be_able_to(:create, AssetResource) }
@@ -36,6 +42,13 @@ describe 'Ability' do
     it { is_expected.to be_able_to(:create, ItemResource) }
     it { is_expected.to be_able_to(:update, ItemResource) }
     it { is_expected.not_to be_able_to(:destroy, ItemResource) }
+
+    it { is_expected.to be_able_to(:read, BulkExport) }
+    it { is_expected.to be_able_to(:create, BulkExport) }
+    it { is_expected.to be_able_to(:update, BulkExport) }
+    it { is_expected.to be_able_to(:destroy, BulkExport) }
+    it { is_expected.not_to be_able_to(:update, bulk_export) }
+    it { is_expected.not_to be_able_to(:destroy, bulk_export) }
 
     it { is_expected.not_to be_able_to(:manage, User) }
   end
