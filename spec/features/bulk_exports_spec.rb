1c98eba2ec344f01633879b5b068f9dd599108f0
Amrey Mathurin <mamrey@upenn.edu>
2023-02-07 09:48:24 -0500

    Update tests - fix typo in spec filename

--- spec/features/bulk_exports_spec.rb
+++ spec/features/bulk_exports_spec.rb
@@ -3,28 +3,24 @@
 
 describe 'BulkExport management' do
 
-  let(:bulk_exports) { create_list(:bulk_export, 10, state: BulkExport::STATE_QUEUED) }
+  let!(:bulk_exports) { create_list(:bulk_export, 10, state: BulkExport::STATE_QUEUED) }
 
   before do
     persist(:item_resource)
-    bulk_exports.each(&:process!)
   end
 
   context 'with a viewer' do
 
-    before { sign_in user }
+    before do
+      sign_in user
+      visit bulk_exports_path
+    end
 
     let(:user) { create(:user, :viewer) }
 
     it 'lists all BulkExports' do
-      visit bulk_exports_path
-      expect(page).to have_text('Solr Query Parameters', count: bulk_exports.length)
+      expect(page).to have_text('Search Parameters', count: bulk_exports.length)
       expect(page).to have_css('.card', count: bulk_exports.length)
-    end
-
-    it 'shows a link to download the attached csv' do
-      visit bulk_exports_path
-      expect(page).to have_link('Download CSV', count: bulk_exports.length)
     end
 
     it 'does not show any buttons' do
@@ -36,16 +32,15 @@ describe 'BulkExport management' do
   context 'with an editor' do
 
     let(:user) { create(:user, :editor) }
-    let(:bulk_export) { create(:bulk_export, user: user, state: BulkExport::STATE_QUEUED) }
+    let!(:bulk_export) { create(:bulk_export, user: user, state: BulkExport::STATE_QUEUED) }
 
     before do
       sign_in user
-      bulk_export.process!
       visit bulk_exports_path
     end
 
     it 'lists all BulkExports' do
-      expect(page).to have_text('Solr Query Parameters', count: bulk_exports.length + 1)
+      expect(page).to have_text('Search Parameters', count: bulk_exports.length + 1)
       expect(page).to have_css('.card', count: bulk_exports.length + 1)
     end
 
@@ -64,12 +59,27 @@ describe 'BulkExport management' do
     end
 
     it 'lists all BulkExports' do
-      expect(page).to have_text('Solr Query Parameters', count: bulk_exports.length)
+      expect(page).to have_text('Search Parameters', count: bulk_exports.length)
       expect(page).to have_css('.card', count: bulk_exports.length)
     end
 
     it 'shows all the buttons' do
       expect(page).to have_button('Export', count: bulk_exports.length * 3)
+    end
+  end
+
+  context 'with a processed bulk export' do
+
+    let(:user) { create(:user, :viewer) }
+
+    before do
+      bulk_exports.each(&:process!)
+      sign_in user
+      visit bulk_exports_path
+    end
+
+    it 'shows a link to download the attached csv' do
+      expect(page).to have_link('Download CSV', count: bulk_exports.length)
     end
   end
 
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -17,15 +17,19 @@ describe 'Ability' do
 
   context 'with a viewer user' do
     let(:user) { create :user, :viewer }
+    let(:bulk_export) { create :bulk_export }
+
 
     it { is_expected.to be_able_to(:read, BulkExport)}
     it { is_expected.to be_able_to(:read, ItemResource) }
     it { is_expected.to be_able_to(:read, AssetResource) }
     it { is_expected.not_to be_able_to(:edit, AssetResource) }
     it { is_expected.not_to be_able_to(:edit, ItemResource) }
-    it { is_expected.not_to be_able_to(:create, BulkExport) }
-    it { is_expected.not_to be_able_to(:update, BulkExport) }
-    it { is_expected.not_to be_able_to(:destroy, BulkExport) }
+    it { is_expected.to be_able_to(:create, BulkExport) }
+    it { is_expected.to be_able_to(:update, BulkExport) }
+    it { is_expected.to be_able_to(:destroy, BulkExport) }
+    it { is_expected.not_to be_able_to(:update, bulk_export) }
+    it { is_expected.not_to be_able_to(:destroy, bulk_export) }
     it { is_expected.not_to be_able_to(:view, User) }
   end
 
