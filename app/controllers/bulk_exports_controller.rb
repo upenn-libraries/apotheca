488459aaf0ee61ada6f0d50dc6153ca586be6539
Amrey Mathurin <mamrey@upenn.edu>
2023-02-13 17:02:18 -0500

    Implement options from collection pattern - write clearer input labels

--- app/controllers/bulk_exports_controller.rb
+++ app/controllers/bulk_exports_controller.rb
@@ -4,7 +4,8 @@
 class BulkExportsController < ApplicationController
   load_and_authorize_resource
   def index
-    @bulk_exports = BulkExport.page(params[:page])
+    @users = User.user_with_exports
+    @bulk_exports = BulkExport.with_user.page(params[:page])
     @bulk_exports = @bulk_exports.filter_user(params[:filter][:user]) if params.dig('filter', 'user').present?
     @bulk_exports = @bulk_exports.sort_by_field(params[:sort][:field], params[:sort][:direction]) if params[:sort].present?
   end
