4907ff30e26136e44ab220964dd290e3a8a3b285
Amrey Mathurin <mamrey@upenn.edu>
2023-02-13 17:39:39 -0500

    Fix typo in scope name

--- app/controllers/bulk_exports_controller.rb
+++ app/controllers/bulk_exports_controller.rb
@@ -4,7 +4,7 @@
 class BulkExportsController < ApplicationController
   load_and_authorize_resource
   def index
-    @users = User.user_with_exports
+    @users = User.with_exports
     @bulk_exports = BulkExport.with_user.page(params[:page])
     @bulk_exports = @bulk_exports.filter_user(params[:filter][:user]) if params.dig('filter', 'user').present?
     @bulk_exports = @bulk_exports.sort_by_field(params[:sort][:field], params[:sort][:direction]) if params[:sort].present?
