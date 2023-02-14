077d0d4a75463e1a51f4af0caab78fd3cd539272
Alyssa Hogan <abhogan@upenn.edu>
2023-02-14 14:58:05 -0500

    Add tests for Import model

--- spec/models/import_spec.rb
+++ spec/models/import_spec.rb
@@ -4,4 +4,16 @@ require_relative 'concerns/queueable'
 
 describe Import do
   it_behaves_like 'queueable'
+
+  it 'requires import data' do
+    import = build :import, import_data: nil
+    expect(import.valid?).to be false
+    expect(import.errors['import_data']).to include "can't be blank"
+  end
+
+  it 'creates an import' do
+    import = create :import
+    expect(import.valid?).to be true
+    expect(import.import_data).to be_a Hash
+  end
 end
