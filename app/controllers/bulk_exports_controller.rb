c204932726d63b7db46722dde974a141fa7beebb
Patrick Perkins <lilblizzard97@gmail.com>
2023-02-16 15:02:03 -0500

    add destroy controller action

--- app/controllers/bulk_exports_controller.rb
+++ app/controllers/bulk_exports_controller.rb
@@ -10,4 +10,14 @@ class BulkExportsController < ApplicationController
     @bulk_exports = @bulk_exports.filter_user(params[:filter][:user]) if params.dig('filter', 'user').present?
     @bulk_exports = @bulk_exports.sort_by_field(params[:sort][:field], params[:sort][:direction]) if params[:sort].present?
   end
+
+  def destroy
+    @bulk_export = BulkExport.find(params[:id])
+    if @bulk_export.processing?
+      redirect_to bulk_exports_path, alert: 'Cannot delete a bulk export that is currently processing.'
+    else
+      @bulk_export.destroy
+      redirect_to bulk_exports_path, notice: 'Bulk export deleted.'
+    end
+  end
 end
