01221a082d4ed18c185f492559d48c68bbd4f4e9
Amrey Mathurin <mamrey@upenn.edu>
2023-02-20 11:08:21 -0500

    Fix trait declaration in User factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,17 +5,17 @@ FactoryBot.define do
     email { Faker::Internet.unique.email }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
-  end
 
-  trait :viewer do
-    roles { [User::VIEWER_ROLE] }
-  end
+    trait :viewer do
+      roles { [User::VIEWER_ROLE] }
+    end
 
-  trait :editor do
-    roles { [User::EDITOR_ROLE] }
-  end
+    trait :editor do
+      roles { [User::EDITOR_ROLE] }
+    end
 
-  trait :admin do
-    roles { [User::ADMIN_ROLE] }
+    trait :admin do
+      roles { [User::ADMIN_ROLE] }
+    end
   end
 end
