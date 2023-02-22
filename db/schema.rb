ce372ad74f7426c7b4c7a9b29df31c19120f0c84
Amrey Mathurin <mamrey@upenn.edu>
2023-02-22 13:37:38 -0500

    Add foreign key constraint to bulk_exports table - enforce no null values in foreign key column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_21_175355) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_22_163233) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
@@ -53,7 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_21_175355) do
   end
 
   create_table "bulk_exports", force: :cascade do |t|
-    t.bigint "created_by_id"
+    t.bigint "created_by_id", null: false
     t.jsonb "search_params"
     t.string "process_errors", array: true
     t.integer "duration"
@@ -117,6 +117,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_21_175355) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bulk_exports", "users", column: "created_by_id"
   add_foreign_key "bulk_imports", "users", column: "created_by_id"
   add_foreign_key "imports", "bulk_imports"
 end
