e2df9febf648a405b23e13844ffac8428ddd6ca5
Patrick Perkins <lilblizzard97@gmail.com>
2023-03-09 11:17:28 -0600

    turn options into constant, implement in filter partials

--- app/controllers/concerns/per_page.rb
+++ app/controllers/concerns/per_page.rb
@@ -3,6 +3,8 @@
 module PerPage
   extend ActiveSupport::Concern
 
+  PER_PAGE_OPTIONS = [10, 25, 50, 100].freeze
+
   included do
     before_action only: :index do
       session[:"#{controller_name}_per_page"] = params[:per_page] unless params[:per_page].nil?
