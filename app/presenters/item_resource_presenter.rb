dd31699224970dbb3bd06615a65c833de56a08b9
Alyssa Hogan <abhogan@upenn.edu>
2023-03-30 08:30:32 -0400

    Side-by-side ILS and Resource descriptive metadata

--- app/presenters/item_resource_presenter.rb
+++ app/presenters/item_resource_presenter.rb
@@ -18,6 +18,9 @@ class ItemResourcePresenter < BasePresenter
 
   # presenter for descriptive metadata
   class DescriptiveMetadataPresenter < BasePresenter
+    include ActionView::Helpers::TagHelper
+    include ActionView::Helpers::TextHelper
+    include ActionView::Context
     attr_accessor :ils_metadata
 
     # define accessors for descriptive metadata fields that first look in ILS metadata, if present
@@ -35,6 +38,49 @@ class ItemResourcePresenter < BasePresenter
     def initialize(object:, ils_metadata: nil)
       super object: object
       @ils_metadata = ils_metadata&.with_indifferent_access
+    end
+
+    # Get field values as unstyled list
+    #
+    # @param [String] source
+    # @param [String] field
+    # @return [String (frozen)]
+    def field_values(source, field)
+      field_values = source == 'resource' ? object[field] : @ils_metadata[field]
+
+      content_tag :ul, class: 'list-unstyled mb-0' do
+        field_values&.each&.with_index do |value, i|
+          concat content_tag :li, value, class: i.zero? ? '' : 'pt-2'
+        end
+      end
+    end
+
+    # Add bootstrap classes to identify whether field's ILS value will be used or overridden
+    # ILS value is only used if field has no resource value
+    #
+    # @param [String] field from ItemResource::DescriptiveMetadata::FIELDS
+    # @return [String (frozen)]
+    def field_ils_class(field)
+      ils_metadata && object[field].empty? ? 'bg-success bg-opacity-10' : 'opacity-75 text-decoration-line-through'
+    end
+
+    # Add bootstrap classes to identify that field's resource value is given precedence over ILS value
+    #
+    # @param [String] field from ItemResource::DescriptiveMetadata::FIELDS
+    # @return [nil] if field does not have an ILS value
+    # @return [String (frozen)] if field has an ILS value
+    def field_resource_class(field)
+      return unless @ils_metadata
+
+      'bg-success bg-opacity-10' if object[field].present?
+    end
+
+    # Check if field has either ILS or resource value (otherwise won't be displayed)
+    #
+    # @param [String] field from ItemResource::DescriptiveMetadata::FIELDS
+    # @return [TrueClass, FalseClass]
+    def field_row_data?(field)
+      (@ils_metadata && @ils_metadata[field].present?) || object[field].present?
     end
   end
 end
