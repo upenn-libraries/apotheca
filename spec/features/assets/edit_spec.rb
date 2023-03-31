06b162f04eb9bacf4d518597db1b3a246618c091
Amrey Mathurin <mamrey@upenn.edu>
2023-03-31 16:58:24 -0400

    Refactor Assets specs

---
+++ spec/features/assets/edit_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+describe 'Asset Edit Page' do
+
+  let(:user) { create(:user, :admin) }
+  let(:item) { persist(:item_resource, :with_asset) }
+  let(:asset_label) { 'Test' }
+
+  before do
+    sign_in user
+    visit edit_asset_path(item.asset_ids.first)
+  end
+
+  it 'requires a preservation file' do
+    fill_in 'asset-label', with: 'Test'
+    click_on 'Save'
+    expect(page).to have_text("Preservation file can't be blank")
+  end
+
+  it 'can attach the preservation file' do
+    attach_file 'asset-file', 'spec/fixtures/files/front.tif'
+    click_on 'Save'
+    expect(page).to have_text('Successfully updated asset.')
+  end
+
+  it 'can update the asset' do
+    fill_in 'asset-label', with: asset_label
+    attach_file 'asset-file', 'spec/fixtures/files/front.tif'
+    click_on 'Save'
+    expect(page).to have_text(asset_label)
+    expect(page).to have_text('Successfully updated asset.')
+  end
+
+end
