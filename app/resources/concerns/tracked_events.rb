007c46d956bb7f21ffc96c275275ab323a0e1e80
Mike Kanning <mkanning@upenn.edu>
2023-04-07 08:22:13 -0400

    use an event() method in TrackedEvents to set common values across event methods.

--- app/resources/concerns/tracked_events.rb
+++ app/resources/concerns/tracked_events.rb
@@ -7,16 +7,18 @@ module TrackedEvents
   SYSTEM_AGENT = 'system_user@upenn.edu' # TODO: how will we denote a system-initiated action?
 
   class_methods do
+    # @param [String] type
+    # @param [String] outcome
     # @param [String] note
     # @param [String] agent
     # @param [DateTime] timestamp
     # @return [AssetResource::PreservationEvent]
-    def checksum(note:, agent:, timestamp:)
+    def event(type:, outcome:, note:, agent:, timestamp:)
       AssetResource::PreservationEvent.new(
         identifier: SecureRandom.uuid,
-        event_type: Premis::Events::CHECKSUM.uri,
+        event_type: type,
         timestamp: timestamp,
-        outcome: Premis::Outcomes::SUCCESS.uri,
+        outcome: outcome,
         outcome_detail_note: note,
         agent: agent,
         agent_type: 'local',
@@ -28,17 +30,24 @@ module TrackedEvents
     # @param [String] agent
     # @param [DateTime] timestamp
     # @return [AssetResource::PreservationEvent]
+    def checksum(note:, agent:, timestamp:)
+      event type: Premis::Events::CHECKSUM.uri,
+            outcome: Premis::Outcomes::SUCCESS.uri,
+            note: note,
+            agent: agent,
+            timestamp: timestamp
+    end
+
+    # @param [String] note
+    # @param [String] agent
+    # @param [DateTime] timestamp
+    # @return [AssetResource::PreservationEvent]
     def filename_changed(agent:, note:, timestamp:)
-      AssetResource::PreservationEvent.new(
-        identifier: SecureRandom.uuid,
-        event_type: Premis::Events::EDIT_FILENAME.uri,
-        timestamp: timestamp,
-        outcome: Premis::Outcomes::SUCCESS.uri,
-        outcome_detail_note: note,
-        agent: agent,
-        agent_type: 'local',
-        agent_role: agent_role(agent)
-      )
+      event type: Premis::Events::EDIT_FILENAME.uri,
+            outcome: Premis::Outcomes::SUCCESS.uri,
+            note: note,
+            agent: agent,
+            timestamp: timestamp
     end
 
     # Used for ingestion, re-ingestion and migration events
@@ -48,34 +57,24 @@ module TrackedEvents
     # @param [DateTime] timestamp
     # @return [AssetResource::PreservationEvent]
     def ingestion(note:, agent:, timestamp:)
-      AssetResource::PreservationEvent.new(
-        identifier: SecureRandom.uuid,
-        event_type: Premis::Events::INGEST.uri,
-        timestamp: timestamp,
-        outcome: Premis::Outcomes::SUCCESS.uri,
-        outcome_detail_note: note,
-        agent: agent,
-        agent_type: 'local',
-        agent_role: agent_role(agent)
-      )
+      event type: Premis::Events::INGEST.uri,
+            outcome: Premis::Outcomes::SUCCESS.uri,
+            note: note,
+            agent: agent,
+            timestamp: timestamp
     end
 
-    # @param [Object] outcome
+    # @param [String] outcome
     # @param [String] note
     # @param [String] agent
-    # @param [DateTime] timestamp
+    # @param [DateTime|nil] timestamp
     # @return [AssetResource::PreservationEvent]
     def virus_check(outcome:, note:, agent:, timestamp: nil)
-      AssetResource::PreservationEvent.new(
-        identifier: SecureRandom.uuid,
-        event_type: Premis::Events::VIRUS_CHECK.uri,
-        timestamp: timestamp,
-        outcome: outcome,
-        outcome_detail_note: note,
-        agent: agent,
-        agent_type: 'local',
-        agent_role: agent_role(agent)
-      )
+      event type: Premis::Events::VIRUS_CHECK.uri,
+            outcome: outcome,
+            note: note,
+            agent: agent,
+            timestamp: timestamp
     end
 
     # TODO: these actions remain to be integrated
