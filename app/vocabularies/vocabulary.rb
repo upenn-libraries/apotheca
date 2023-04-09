45cce445b3630a87c6279683a49b28ad0f9c5e9e
Mike Kanning <mkanning@upenn.edu>
2023-04-09 08:39:01 -0400

    use familiar hash param syntax for Vocabulary.find_by. Support and test handling of multiple conditions. Improve usage documentation.

--- app/vocabularies/vocabulary.rb
+++ app/vocabularies/vocabulary.rb
@@ -1,22 +1,29 @@
 # frozen_string_literal: true
 
 # Base class for building Vocabularies
+# Define common Data objects here on Types
+# Subclasses should list term values as Data objects, e.g.:
+# class MyTerms < Vocabulary
+#   Heading = Data.define :label, :uri, :active
+#   MY_HEADING = Heading['Stuff', 'http://stuff.com/pid/1234', false]
+# end
 class Vocabulary
   module Types
     # Data objects available for use in Vocabulary subclasses
     Term = Data.define :label, :uri
   end
 
-  # Dynamic lookup for use in child classes
-  # @param [String|Symbol] field
-  # @param [Object] value
+  # Dynamic lookup of constant values for use in child classes
   # @return [Object]
-  def self.find_by(field, value)
+  # @param [Hash] conditions
+  def self.find_by(conditions)
     term_name = constants(false).find do |constant_name|
       const = const_get(constant_name)
-      next unless const.respond_to?(field.to_sym)
+      conditions.all? do |k, v|
+        return nil unless const.respond_to?(k.to_sym)
 
-      const.public_send(field.to_sym) == value
+        const.public_send(k.to_sym) == v
+      end
     end
     return nil unless term_name
 
--- spec/vocabularies/vocabulary_spec.rb
+++ spec/vocabularies/vocabulary_spec.rb
@@ -3,24 +3,28 @@
 describe Vocabulary do
   subject(:vocabulary) { described_class }
 
-  let(:dummy_class) { Class.new(described_class) }
-
-  before do
-    term = Data.define :label, :value
-    dummy_class.const_set :TEST_TERM, term['cheesy', 'crunchy']
+  let(:dummy_class) do
+    Class.new(described_class) do |klass|
+      term = Data.define :label, :value
+      klass.const_set :TEST_TERM, term['cheesy', 'crunchy']
+    end
   end
 
   describe '.find_by' do
     it 'returns nil if constant is not defined' do
-      expect(dummy_class.find_by(:label, 'none')).to be_nil
+      expect(dummy_class.find_by(label: 'none')).to be_nil
     end
 
     it "returns nil if field doesn't exist on term type" do
-      expect(dummy_class.find_by(:quack, 'duck')).to be_nil
+      expect(dummy_class.find_by(label: 'duck')).to be_nil
     end
 
-    it 'returns the value of the constant if a match is found' do
-      expect(dummy_class.find_by(:label, 'cheesy')).to be_a Data
+    it 'returns the value of the constant if a match is found using a single condition' do
+      expect(dummy_class.find_by(label: 'cheesy')).to be_a Data
+    end
+
+    it 'returns the value of the constant if a match is found with multiple conditions' do
+      expect(dummy_class.find_by(label: 'cheesy', value: 'crunchy')).to be_a Data
     end
   end
 end
