4b9bc641bbfd7a58e2bfb579002c51e66ab79c6b
Amrey Mathurin <mamrey@upenn.edu>
2023-04-11 17:29:55 -0400

    Improve logic to disable submit button after form submitted - Implement helper method to add data attributes to options hash (cgalarza) - reorder data attribute configuration to apply Stimulus actions in correct order - allow disable behavior to be turned off

--- app/components/form/submit_button/component.rb
+++ app/components/form/submit_button/component.rb
@@ -9,29 +9,36 @@ module Form
       # @param [String] disable_with value for disabled version of submit button when form is submitted, optional
       # @param [String, TrueClass, FalseClass] confirm can be a String message for display or just true for a standard
       # message
-      def initialize(value, variant: :primary, disable_with: nil, confirm: false, **options)
+      def initialize(value, variant: :primary, disable_with: 'Processing...', confirm: false, **options)
         @value = value
         @options = options
         @disable_with = disable_with
         @confirm = confirm
         @options[:class] = Array.wrap(@options[:class]).push('btn', "btn-#{variant}")
         @options[:data] = @options.fetch(:data, {})
-        configure_form_disable
         configure_confirmation if confirm
+        configure_form_disable if disable_with
       end
 
       def configure_confirmation
         message = @confirm.is_a?(String) ? @confirm : 'Are you sure?'
-        confirm_action = 'click->form--submit-button--submit#confirm'
-        @options[:data].merge!({ confirm: message,
-                                 action: "#{confirm_action} #{@options[:data][:action]}" })
+        add_data_attributes(controller: 'form--submit-button--submit',
+                            confirm: message,
+                            action: 'click->form--submit-button--submit#confirm')
       end
 
       def configure_form_disable
         disable_with_value = @disable_with.is_a?(String) ? @disable_with : 'Processing...'
-        @options[:data].merge!({ controller: 'form--submit-button--submit',
-                                 action: 'click->form--submit-button--submit#disableSubmit',
-                                 'disable-with': disable_with_value })
+        add_data_attributes(controller: 'form--submit-button--submit',
+                            action: 'click->form--submit-button--submit#disableSubmit',
+                            'disable-with': disable_with_value)
+      end
+
+      # add data attributes to options hash while maintaining order of multiple Stimulus actions
+      def add_data_attributes(**attributes)
+        @options[:data].merge!(attributes) do |k, old_value, new_value|
+          k == :action ? "#{old_value} #{new_value}" : new_value
+        end
       end
 
       def call
