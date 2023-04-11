26884b3e69274df2b328f13470e3a38512472fea
Amrey Mathurin <mamrey@upenn.edu>
2023-04-11 13:10:23 -0400

    Implement form disable logic on SubmitButton Component and Submit Controller - allow submit_controller.js to handle disabling submit button - Change default behavior of form submission, submit button now disabled whenever form submitted - refactor confirmation logic in light of expanded responsibility of submit_controller.js

--- app/components/form/submit_button/component.rb
+++ app/components/form/submit_button/component.rb
@@ -6,23 +6,32 @@ module Form
     class Component < ViewComponent::Base
       # @param [String] value
       # @param [Symbol] variant
+      # @param [String] disable_with value for disabled version of submit button when form is submitted, optional
       # @param [String, TrueClass, FalseClass] confirm can be a String message for display or just true for a standard
       # message
-      def initialize(value, variant: :primary, confirm: false, **options)
+      def initialize(value, variant: :primary, disable_with: nil, confirm: false, **options)
         @value = value
         @options = options
-
+        @disable_with = disable_with
         @confirm = confirm
         @options[:class] = Array.wrap(@options[:class]).push('btn', "btn-#{variant}")
         @options[:data] = @options.fetch(:data, {})
+        configure_form_disable
         configure_confirmation if confirm
       end
 
       def configure_confirmation
         message = @confirm.is_a?(String) ? @confirm : 'Are you sure?'
+        confirm_action = 'click->form--submit-button--submit#confirm'
         @options[:data].merge!({ confirm: message,
-                                 controller: 'form--submit-button--submit',
-                                 action: 'click->form--submit-button--submit#confirm' })
+                                 action: "#{confirm_action} #{@options[:data][:action]}" })
+      end
+
+      def configure_form_disable
+        disable_with_value = @disable_with.is_a?(String) ? @disable_with : 'Processing...'
+        @options[:data].merge!({ controller: 'form--submit-button--submit',
+                                 action: 'click->form--submit-button--submit#disableSubmit',
+                                 'disable-with': disable_with_value })
       end
 
       def call
--- app/components/form/submit_button/submit_controller.js
+++ app/components/form/submit_button/submit_controller.js
@@ -5,6 +5,17 @@ export default class extends Controller {
         const message = this.element.dataset.confirm ;
         if(!confirm(message)) {
             event.preventDefault();
+            // stop stimulus action chain for 'click' event
+            event.stopImmediatePropagation();
         }
+    }
+
+    disableSubmit() {
+        const disableWith = this.element.dataset.disableWith
+        // disable submit button after event fires
+        setTimeout(()=> {
+            this.element.disabled = true
+            this.element.value = disableWith
+        },0)
     }
 }
