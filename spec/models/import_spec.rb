66080ac7829ebbc0b0b32449e67a779aa2c76474
Patrick Perkins <lilblizzard97@gmail.com>
2023-04-14 11:07:34 -0400

    test import run method

--- spec/models/import_spec.rb
+++ spec/models/import_spec.rb
@@ -22,4 +22,28 @@ describe Import do
     expect(import.valid?).to be true
     expect(import.import_data).to be_a Hash
   end
+
+  describe '#run' do
+    include_context 'with successful requests to mint EZID'
+    include_context 'with successful requests to update EZID'
+
+    let(:bulk_import) { create(:bulk_import) }
+    let(:import) { create(:import, :processing, bulk_import: bulk_import) }
+
+    before do
+      import.run
+    end
+
+    it 'is successful' do
+      expect(import.state).to eq described_class::STATE_SUCCESSFUL.to_s
+    end
+
+    it 'calculates and stores duration' do
+      expect(import.duration).not_to be_nil
+    end
+
+    it 'sets resource identifier' do
+      expect(import.resource_identifier).not_to be_nil
+    end
+  end
 end
