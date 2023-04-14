3ffbd03b1ba7ba05f128bc7aef4a3ca86da9c5a6
Patrick Perkins <lilblizzard97@gmail.com>
2023-04-14 15:03:42 -0400

    reformat file lister with prettier

--- app/components/file_lister/file_lister_controller.js
+++ app/components/file_lister/file_lister_controller.js
@@ -1,65 +1,75 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ["pathInput", "driveSelect", "filenameList", "extractedFilenamesForm", "extractedFilenamesDrive", "extractedFilenamesPath", "errorMessage"]
+    static targets = [
+        'pathInput',
+        'driveSelect',
+        'filenameList',
+        'extractedFilenamesForm',
+        'extractedFilenamesDrive',
+        'extractedFilenamesPath',
+        'errorMessage',
+    ];
 
-    setDrive(drive){
-        this.extractedFilenamesDriveTarget.value = drive
+    setDrive(drive) {
+        this.extractedFilenamesDriveTarget.value = drive;
     }
 
-    setPath(path){
-        this.extractedFilenamesPathTarget.value = path
+    setPath(path) {
+        this.extractedFilenamesPathTarget.value = path;
     }
 
     setList(filenames) {
-        this.filenameListTarget.innerText = filenames
+        this.filenameListTarget.innerText = filenames;
     }
 
-    setError(error){
-        this.errorMessageTarget.innerText = error
+    setError(error) {
+        this.errorMessageTarget.innerText = error;
     }
 
-    clear(){
-        this.filenameListTarget.innerText = ""
-        this.errorMessageTarget.innerText = ""
+    clear() {
+        this.filenameListTarget.innerText = '';
+        this.errorMessageTarget.innerText = '';
     }
 
     copy() {
-        navigator.clipboard.writeText(this.filenameListTarget.value)
+        navigator.clipboard.writeText(this.filenameListTarget.value);
     }
 
     async getFilenames() {
-        const response = await fetch("/file_listing_tool/file_list", {
-            method: "POST",
+        const response = await fetch('/file_listing_tool/file_list', {
+            method: 'POST',
             headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
             },
-            body: JSON.stringify({drive: this.driveSelectTarget.value, path: this.pathInputTarget.value})
-        })
+            body: JSON.stringify({
+                drive: this.driveSelectTarget.value,
+                path: this.pathInputTarget.value,
+            }),
+        });
 
-        const json = await response.json()
-        return json
+        const json = await response.json();
+        return json;
     }
 
-    async submit(event){
-        event.preventDefault()
-        this.clear()
+    async submit(event) {
+        event.preventDefault();
+        this.clear();
 
-        const json = await this.getFilenames()
+        const json = await this.getFilenames();
 
-        if(json.error){
-            this.setError(json.error)
+        if (json.error) {
+            this.setError(json.error);
         }
 
         if (json.filenames) {
-            this.setDrive(json.drive)
-            this.setPath(json.path)
-            this.setList(json.filenames)
-            this.extractedFilenamesFormTarget.hidden = false
+            this.setDrive(json.drive);
+            this.setPath(json.path);
+            this.setList(json.filenames);
+            this.extractedFilenamesFormTarget.hidden = false;
         } else {
-            this.extractedFilenamesFormTarget.hidden = true
+            this.extractedFilenamesFormTarget.hidden = true;
         }
     }
-
 }
