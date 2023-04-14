236fb461e0b9c242a402c995f4b864369c87ca06
Patrick Perkins <lilblizzard97@gmail.com>
2023-04-14 14:51:07 -0400

    cleanup, getFilenames in its own function

--- app/components/file_lister/file_lister_controller.js
+++ app/components/file_lister/file_lister_controller.js
@@ -3,18 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static targets = ["pathInput", "driveSelect", "filenameList", "fileListingForm", "driveResponse", "pathResponse", "errorMessage"]
 
-    connect(event) {
-        console.log('hello', this.element)
-    }
-
-    getPath(){
-        return this.pathInputTarget.value
-    }
-
-   getDrive() {
-       return this.driveSelectTarget.value
-    }
-
     setDrive(drive){
         this.driveResponseTarget.value = drive
     }
@@ -40,21 +28,27 @@ export default class extends Controller {
         navigator.clipboard.writeText(this.filenameListTarget.value)
     }
 
-    async submit(event){
-        event.preventDefault()
-        this.clear()
+    async getFilenames() {
         const response = await fetch("/file_listing_tool/file_list", {
             method: "POST",
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({drive: this.getDrive(), path: this.getPath()} )
+            body: JSON.stringify({drive: this.driveSelectTarget.value, path: this.pathInputTarget.value})
         })
 
         const json = await response.json()
+        return json
+    }
 
-        if(!response.ok && json.error){
+    async submit(event){
+        event.preventDefault()
+        this.clear()
+
+        const json = await this.getFilenames()
+
+        if(json.error){
             this.setError(json.error)
         }
 
@@ -67,4 +61,5 @@ export default class extends Controller {
             this.fileListingFormTarget.hidden = true
         }
     }
+
 }
