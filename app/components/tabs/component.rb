7a7db681d600bbbbd6a9b6c2b7383fcf1509a741
Amrey Mathurin <mamrey@upenn.edu>
2023-04-17 13:24:58 -0400

    Allow us to implement disabled Tab components

--- app/components/tabs/component.rb
+++ app/components/tabs/component.rb
@@ -10,13 +10,14 @@ module Tabs
     end
 
     class Tab < ViewComponent::Base
-      attr_reader :id, :active, :title, :count
+      attr_reader :id, :active, :title, :count, :disabled
 
-      def initialize(title:, count: nil, active: false)
+      def initialize(title:, count: nil, active: false, disabled: false)
         @id = title.downcase.gsub(' ', '-')
         @title = title
         @active = active
         @count = count
+        @disabled = disabled
       end
 
       def call
