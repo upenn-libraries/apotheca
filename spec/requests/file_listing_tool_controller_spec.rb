c05789367b956704fbbdb1184eabc1ea4a37e3af
Patrick Perkins <lilblizzard97@gmail.com>
2023-04-21 13:34:19 -0400

    dry up file controller spec

--- spec/requests/file_listing_tool_controller_spec.rb
+++ spec/requests/file_listing_tool_controller_spec.rb
@@ -1,20 +1,23 @@
 # frozen_string_literal: true
 
 describe 'file listing tool requests' do
-  before { sign_in create(:user, :viewer) }
+  let(:params) { { drive: 'sceti_digitized', path: '/' } }
+
+  before do
+    sign_in create(:user, :viewer)
+    post file_listing_tool_file_list_path, as: :json, params: params
+  end
 
   context 'with a valid path' do
     it 'returns file list' do
-      params = { drive: 'sceti_digitized', path: '/' }
-      post file_listing_tool_file_list_path, as: :json, params: params
       expect(response.parsed_body['filenames']).to eq('bell.wav; video.mov')
     end
   end
 
   context 'with an invalid path' do
+    let(:params) { { drive: 'invalid_drive', path: '/' } }
+
     it 'returns invalid path error' do
-      params = { drive: 'invalid_drive', path: '/' }
-      post file_listing_tool_file_list_path, as: :json, params: params
       expect(response.parsed_body['error']).to eq('Path invalid!')
     end
   end
