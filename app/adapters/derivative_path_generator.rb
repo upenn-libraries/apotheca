75cbc1fb79eba1cc65ac6b309df59883c7745cd6
Patrick Perkins <lilblizzard97@gmail.com>
2023-04-25 14:14:57 -0400

    app/adapters fixes

--- app/adapters/derivative_path_generator.rb
+++ app/adapters/derivative_path_generator.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 class DerivativePathGenerator
   def initialize(base_path: nil)
     @base_path = base_path
   end
 
   def generate(resource:, file:, original_filename:)
-    raise ArgumentError, "original_filename must be provided" unless original_filename
-    "#{resource.id.to_s}/#{original_filename}"
+    raise ArgumentError, 'original_filename must be provided' unless original_filename
+
+    "#{resource.id}/#{original_filename}"
   end
 end
