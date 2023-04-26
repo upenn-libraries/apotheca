64182dd0da55eb2cb612a1ddd5ee7f6536435e1c
Patrick Perkins <lilblizzard97@gmail.com>
2023-04-26 15:07:59 -0400

    split invalid path and drive into separate tests

--- spec/requests/file_listing_tool_controller_spec.rb
+++ spec/requests/file_listing_tool_controller_spec.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 describe 'file listing tool requests' do
-
   before do
     sign_in create(:user, :viewer)
     post file_listing_tool_file_list_path, as: :json, params: params
   end
 
-  context 'with a valid path' do
+  context 'with a valid drive and path' do
     let(:params) { { drive: 'sceti_digitized', path: '/' } }
 
     it 'returns file list' do
@@ -16,6 +15,14 @@ describe 'file listing tool requests' do
   end
 
   context 'with an invalid path' do
+    let(:params) { { drive: 'sceti_digitized', path: '/invalid_path' } }
+
+    it 'returns invalid path error' do
+      expect(response.parsed_body['error']).to eq('Path invalid!')
+    end
+  end
+
+  context 'with an invalid drive' do
     let(:params) { { drive: 'invalid_drive', path: '/' } }
 
     it 'returns invalid path error' do
