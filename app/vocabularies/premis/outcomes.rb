ff02fa6b8d15f57f3eedf22039356714420ffe39
Patrick Perkins <lilblizzard97@gmail.com>
2023-04-27 17:13:37 -0400

    app/vocabularies fixes

--- app/vocabularies/premis/outcomes.rb
+++ app/vocabularies/premis/outcomes.rb
@@ -4,7 +4,6 @@ module Premis
   # PREMIS Outcomes
   # https://id.loc.gov/vocabulary/preservation/eventOutcome.html
   class Outcomes < Vocabulary
-
     FAILURE = Types::Term['fail',    'http://id.loc.gov/vocabulary/preservation/eventOutcome/fai']
     SUCCESS = Types::Term['success', 'http://id.loc.gov/vocabulary/preservation/eventOutcome/suc']
     WARNING = Types::Term['warning', 'http://id.loc.gov/vocabulary/preservation/eventOutcome/war']
--- app/vocabularies/premis/roles.rb
+++ app/vocabularies/premis/roles.rb
@@ -4,7 +4,6 @@ module Premis
   # PREMIS Roles
   # https://id.loc.gov/vocabulary/preservation/eventRelatedAgentRole.html
   class Roles < Vocabulary
-
     IMPLEMENTER = Types::Term['implementer',
                               'http://id.loc.gov/vocabulary/preservation/eventRelatedAgentRole/imp']
     PROGRAM =     Types::Term['executing program',
