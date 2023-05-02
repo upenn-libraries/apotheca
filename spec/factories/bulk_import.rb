a6a776cd1049b43af238831914ea45e07828340c
Amrey Mathurin <mamrey@upenn.edu>
2023-05-02 16:58:03 -0400

    Add default csv_rows attribute to bulk import factory

--- spec/factories/bulk_import.rb
+++ spec/factories/bulk_import.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
   factory :bulk_import do
     association :created_by, factory: [:user, :admin]
     original_filename { Faker::File.file_name(ext: 'csv', directory_separator: '') }
+    csv_rows do
+      csv_data = Rails.root.join('spec/fixtures/imports/bulk_import_data.csv').read
+      StructuredCSV.parse(csv_data)
+    end
   end
 end
