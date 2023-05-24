0ad094ad9ace2c16431ba08ffa2736d3e5787cf6
Carla Galarza <cgalarza@upenn.edu>
2023-05-24 10:55:40 -0400

    Adding preservation events step to create asset transaction to account for the use case where original filename could be set on create

--- app/transactions/create_asset.rb
+++ app/transactions/create_asset.rb
@@ -5,6 +5,7 @@ class CreateAsset
 
   step :create_change_set, with: 'asset_resource.create_change_set'
   step :set_updated_by, with: 'change_set.set_updated_by'
+  step :add_preservation_events, with: 'asset_resource.add_preservation_events'
   step :validate, with: 'change_set.validate'
   step :save, with: 'change_set.save'
 end
--- spec/transactions/create_asset_spec.rb
+++ spec/transactions/create_asset_spec.rb
@@ -3,33 +3,55 @@
 describe CreateAsset do
   describe '#call' do
     let(:transaction) { described_class.new }
+    let(:asset) { result.value! }
 
     context 'when all required attributes present' do
-      subject(:asset) { result.value! }
-
       let(:result) do
-        transaction.call(
-          original_filename: 'front.jpeg',
-          label: 'Front',
-          created_by: 'admin@example.com'
-        )
+        transaction.call(label: 'Front', created_by: 'admin@example.com')
       end
 
       it 'is successful' do
         expect(result.success?).to be true
       end
 
-      it 'creates a new Asset Resource' do
+      it 'creates a new Asset resource' do
         expect(asset).to be_a AssetResource
       end
 
       it 'sets attributes' do
-        expect(asset.original_filename).to eql 'front.jpeg'
         expect(asset.label).to eql 'Front'
       end
 
       it 'sets updated_by' do
         expect(asset.updated_by).to eql 'admin@example.com'
+      end
+
+      it 'does not add preservation event' do
+        expect(asset.preservation_events.count).to be 0
+      end
+    end
+
+    context 'when creating an asset with an original_filename' do
+      let(:result) do
+        transaction.call(original_filename: original_filename, label: 'Front', created_by: 'admin@example.com')
+      end
+      let(:original_filename) { 'front.tif' }
+
+      it 'is successful' do
+        expect(result.success?).to be true
+      end
+
+      it 'creates new Asset resource' do
+        expect(asset).to be_a AssetResource
+      end
+
+      it 'sets original_filename' do
+        expect(asset.original_filename).to eql original_filename
+      end
+
+      it 'adds preservation event' do
+        expect(asset.preservation_events.count).to be 1
+        expect(asset.preservation_events.first.event_type.to_s).to eql Premis::Events::FILENAME_CHANGE.uri
       end
     end
   end
