b53a6b3e3ad7c1946431071c067b93fd65f795fd
Carla Galarza <cgalarza@upenn.edu>
2023-07-27 15:48:10 -0400

    Additional MARC mapping changes requested by staff

--- rails_app/spec/services/metadata_extractor/marmite/transformer_spec.rb
+++ rails_app/spec/services/metadata_extractor/marmite/transformer_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe MetadataExtractor::Marmite::Transformer do
     context 'when record is a book' do
       let(:expected_metadata) do
         {
-          coverage: [{ value: 'Early works to 1800.' }],
+          coverage: [{ value: 'Early works to 1800' }],
           language: [{ value: 'German', uri: 'https://id.loc.gov/vocabulary/iso639-2/ger' }],
           note: [
             { value: 'Leaves printed on both sides.' },
@@ -20,7 +20,7 @@ RSpec.describe MetadataExtractor::Marmite::Transformer do
             { value: 'Online version available via Colenda' },
             { value: 'Penn Libraries copy has Edgar Fahs Smith\'s autograph on front free endpaper; autograph of H. Wright on front free endpaper; effaced ms. inscription (autograph?) on title leaf.' }
           ],
-          extent: [{ value: '4 unnumbered leaves, 134 leaves, 4 unnumbered leaves :illustrations ;31 cm (folio)' }],
+          extent: [{ value: '4 unnumbered leaves, 134 leaves, 4 unnumbered leaves : illustrations ; 31 cm (folio)' }],
           item_type: [{ value: 'Text', uri: 'http://purl.org/dc/dcmitype/Text' }],
           location: [{ value: 'Germany -- Frankfurt am Main.' }],
           publisher: [{ value: 'Durch Johan Feyerabendt' }],
@@ -39,12 +39,12 @@ RSpec.describe MetadataExtractor::Marmite::Transformer do
           name: [
             { value: 'Ercker, Lazarus, -1594', uri: 'http://id.loc.gov/authorities/names/n85215805' },
             { value: 'Feyerabend, Johann, 1550-1599', uri: 'http://id.loc.gov/authorities/names/nr95034041',
-              role: [{ value: 'printer.' }] }
+              role: [{ value: 'printer' }] }
           ],
           collection: [{ value: 'Edgar Fahs Smith Memorial Collection (University of Pennsylvania)' }],
-          physical_location: [{ value: 'KislakCntr scsmith Folio TN664 .E7 1598' }],
+          physical_location: [{ value: 'Kislak Center for Special Collections, Rare Books and Manuscripts, E.F. Smith Collection, Folio TN664 .E7 1598' }],
           title: [
-            { value: 'Beschreibung aller fürnemisten Mineralischen Ertzt vnnd Berckwercksarten : wie dieselbigen vnd eine jede in Sonderheit jrer Natur vnd Eygenschafft nach, auff alle Metalla probirt, vnd im kleinen Fewr sollen versucht werden, mit Erklärung etlicher fürnemer nützlicher Schmeltzwerck im grossen Feuwer, auch Scheidung Goldts, Silbers, vnd anderer Metalln, sampt einem Bericht des Kupffer Saigerns, Messing brennens, vnd Salpeter Siedens, auch aller saltzigen Minerischen proben, vnd was denen allen anhengig : in fünff Bücher verfast, dessgleichen zuvorn niemals in Druck kommen ... : auffs newe an vielen Orten mit besserer Aussführung, vnd mehreren Figurn erklärt /' }
+            { value: 'Beschreibung aller fürnemisten Mineralischen Ertzt vnnd Berckwercksarten : wie dieselbigen vnd eine jede in Sonderheit jrer Natur vnd Eygenschafft nach, auff alle Metalla probirt, vnd im kleinen Fewr sollen versucht werden, mit Erklärung etlicher fürnemer nützlicher Schmeltzwerck im grossen Feuwer, auch Scheidung Goldts, Silbers, vnd anderer Metalln, sampt einem Bericht des Kupffer Saigerns, Messing brennens, vnd Salpeter Siedens, auch aller saltzigen Minerischen proben, vnd was denen allen anhengig : in fünff Bücher verfast, dessgleichen zuvorn niemals in Druck kommen ... : auffs newe an vielen Orten mit besserer Aussführung, vnd mehreren Figurn erklärt' }
           ]
         }
       end
@@ -62,11 +62,10 @@ RSpec.describe MetadataExtractor::Marmite::Transformer do
           description: [{ value: "Beginning of Sigebert of Gembloux's continuation of the chronicle of Jerome, in which he traces the reigns of kings of various kingdoms.  The last reference is to Pope Zosimus (417 CE; f. 6v)." }],
           item_type: [{ value: 'Text', uri: 'http://purl.org/dc/dcmitype/Text' }],
           language: [{ value: 'Latin', uri: 'https://id.loc.gov/vocabulary/iso639-2/lat' }],
-          extent: [{ value: '10 leaves :paper ;263 x 190 mm bound to 218 x 155 mm' }],
+          extent: [{ value: '10 leaves : paper ; 263 x 190 mm bound to 218 x 155 mm' }],
           name: [
             { value: 'Sigebert, of Gembloux, approximately 1030-1112',
               uri: 'http://id.loc.gov/authorities/names/n87881954' },
-            { value: 'Sigebert, of Gembloux, approximately 1030-1112' }
           ],
           note: [
             { value: 'Ms. gathering.' },
@@ -79,7 +78,7 @@ RSpec.describe MetadataExtractor::Marmite::Transformer do
             { value: 'Origin:  Probably written in Belgium, possibly in Gembloux (inscription on title page of printed work, Bibliotheca Gemblacensis), in the late 15th century (Zacour-Hirsch).' },
             { value: 'Latin.' }
           ],
-          physical_location: [{ value: 'KislakCntr scmss Folio GrC St812 Ef512g' }],
+          physical_location: [{ value: 'Kislak Center for Special Collections, Rare Books and Manuscripts, Manuscripts, Folio GrC St812 Ef512g' }],
           physical_format: [
             { value: 'Chronicles', uri: 'http://vocab.getty.edu/aat/300026361' },
             { value: 'Manuscripts, Latin' },
@@ -87,13 +86,13 @@ RSpec.describe MetadataExtractor::Marmite::Transformer do
           ],
           provenance: [{ value: 'Sold by Bernard M. Rosenthal (New York), 1964.' }],
           relation: [{ value: 'Digital facsimile for browsing (Colenda): https://colenda.library.upenn.edu/catalog/81431-p3833nf29' }],
-          coverage: [{ value: 'Early works to 1800.' }],
+          coverage: [{ value: 'Early works to 1800' }],
           subject: [
             { value: 'World history -- Early works to 1800', uri: 'http://id.loc.gov/authorities/subjects/sh85148202' },
             { value: 'World history', uri: 'http://id.worldcat.org/fast/1181345' }
           ],
           alt_title: [{ value: 'Initium Chronici Siceberti.' }],
-          title: [{ value: '[Partial copy of Chronicon].' }]
+          title: [{ value: '[Partial copy of Chronicon]' }]
         }
       end
       let(:xml) { File.read(file_fixture('marmite/marc_xml/manuscript-1.xml')) }
@@ -160,7 +159,7 @@ RSpec.describe MetadataExtractor::Marmite::Transformer do
       it 'extracts title and transliterated title' do
         expect(
           transformer.to_descriptive_metadata[:title].pluck(:value)
-        ).to contain_exactly('Keian Taiheiki.', '慶安太平記')
+        ).to contain_exactly('Keian Taiheiki', '慶安太平記')
       end
     end
   end
