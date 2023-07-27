35b4c6c2c6879e64b4d9a596679154ee6ead01ea
Patrick Perkins <lilblizzard97@gmail.com>
2023-07-27 15:14:19 -0400

    move some presentation logic to component

--- rails_app/app/components/item_descriptive_metadata/component.rb
+++ rails_app/app/components/item_descriptive_metadata/component.rb
@@ -5,6 +5,44 @@ module ItemDescriptiveMetadata
     def initialize(descriptive_metadata_presenter:)
       @descriptive_metadata_presenter = descriptive_metadata_presenter
     end
+
+    # Get field values as unstyled list
+    #
+    # @param [String] source (ILS vs resource value)
+    # @param [String] field from ItemResource::DescriptiveMetadata::FIELDS
+    # @return [String (frozen)]
+    def field_values(source, field)
+      field_values = source == 'resource' ? @descriptive_metadata_presenter.resource_json_metadata[field] : @descriptive_metadata_presenter.ils_metadata[field]
+
+      tag.ul(class: 'list-unstyled mb-0') do
+        field_values&.each_with_index do |value, i|
+          concat tag.li(class: i.zero? ? '' : 'pt-2') { field_display(value) }
+        end
+      end
+    end
+
+
+    def field_display(value)
+      subfields = [value[:value]]
+      subfields << tag.span(value[:uri], class: 'px-1 small text-secondary') if value[:uri]
+
+      # TODO: This needs a refactor
+      value.except(:value, :uri).each do |k, v|
+        subfields << tag.table(class: ['table', 'table-borderless', 'mb-0']) do
+          tag.tbody do
+            tag.tr do
+              tag.th(k.to_s.titleize, scope: :row) + tag.td do
+                tag.ul(class: 'list-unstyled mb-0') do
+                  safe_join(v.map { |t| tag.li(field_display(t)) })
+                end
+              end
+            end
+          end
+        end
+      end
+
+      safe_join(subfields)
+    end
   end
 end
 
--- rails_app/app/presenters/item_resource_presenter.rb
+++ rails_app/app/presenters/item_resource_presenter.rb
@@ -42,43 +42,6 @@ class ItemResourcePresenter < BasePresenter
       @resource_json_metadata = object.to_json_export.with_indifferent_access
     end
 
-    # Get field values as unstyled list
-    #
-    # @param [String] source (ILS vs resource value)
-    # @param [String] field from ItemResource::DescriptiveMetadata::FIELDS
-    # @return [String (frozen)]
-    def field_values(source, field)
-      field_values = source == 'resource' ? resource_json_metadata[field] : ils_metadata[field]
-
-      tag.ul(class: 'list-unstyled mb-0') do
-        field_values&.each_with_index do |value, i|
-          concat tag.li(class: i.zero? ? '' : 'pt-2') { field_display(value) }
-        end
-      end
-    end
-
-    def field_display(value)
-      subfields = [value[:value]]
-      subfields << tag.span(value[:uri], class: 'px-1 small text-secondary') if value[:uri]
-
-      # TODO: This needs a refactor
-      value.except(:value, :uri).each do |k, v|
-        subfields << tag.table(class: ['table', 'table-borderless', 'mb-0']) do
-          tag.tbody do
-            tag.tr do
-              tag.th(k.to_s.titleize, scope: :row) + tag.td do
-                tag.ul(class: 'list-unstyled mb-0') do
-                  safe_join(v.map { |t| tag.li(field_display(t)) })
-                end
-              end
-            end
-          end
-        end
-      end
-
-      safe_join(subfields)
-    end
-
     # Add bootstrap classes to identify whether field's ILS value will be used or overridden
     # (ILS value only used if field has no resource value)
     #
