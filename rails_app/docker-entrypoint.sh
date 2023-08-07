67965b7717e27f6c774d7189c26f4c0bd8f4df28
Carla Galarza <cgalarza@upenn.edu>
2023-08-07 12:17:17 -0400

    Renaming default database for development. Creating and migrating test database in docker entrypoint file

--- rails_app/docker-entrypoint.sh
+++ rails_app/docker-entrypoint.sh
@@ -26,6 +26,9 @@ if [ "$1" = "bundle" -a "$2" = "exec" -a "$3" = "puma" ] || [ "$1" = "bundle" -a
         bundle exec rake db:migrate
 
         if [ "${RAILS_ENV}" = "development" ]; then
+            bundle exec rake db:create RAILS_ENV=test
+            bundle exec rake db:migrate RAILS_ENV=test
+
             bundle exec rake apotheca:create_buckets
             bundle exec rake apotheca:create_buckets RAILS_ENV=test
         fi
