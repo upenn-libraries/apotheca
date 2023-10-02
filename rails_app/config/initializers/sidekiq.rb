7bccadbbdba6883f84ecc32d1e8eb78dd61c6f8f
Carla Galarza <cgalarza@upenn.edu>
2023-10-02 12:29:12 -0400

    Only configuring sidekiq in non-test environments

--- rails_app/config/initializers/sidekiq.rb
+++ rails_app/config/initializers/sidekiq.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 Rails.application.config.to_prepare do
-  redis_connection = {
-    url: ENV.fetch('REDIS_URL', 'localhost:6379'),
-    username: ENV.fetch('REDIS_SIDEKIQ_USER', 'sidekiq'),
-    password: DockerSecrets.lookup(:redis_sidekiq_password)
-  }
+  unless Rails.env.test?
+    redis_connection = {
+      url: ENV.fetch('REDIS_URL', 'localhost:6379'),
+      username: ENV.fetch('REDIS_SIDEKIQ_USER', 'sidekiq'),
+      password: DockerSecrets.lookup(:redis_sidekiq_password)
+    }
 
-  Sidekiq.default_job_options = { retry: 3, backtrace: 5 }
+    Sidekiq.default_job_options = { retry: 3, backtrace: 5 }
 
-  Sidekiq.configure_server do |config|
-    config.redis = redis_connection
-    config.error_handlers << proc { |e, context| Honeybadger.notify(e, context: context) }
-  end
+    Sidekiq.configure_server do |config|
+      config.redis = redis_connection
+      config.error_handlers << proc { |e, context| Honeybadger.notify(e, context: context) }
+    end
 
-  Sidekiq.configure_client do |config|
-    config.redis = redis_connection
+    Sidekiq.configure_client do |config|
+      config.redis = redis_connection
+    end
   end
 end
 
