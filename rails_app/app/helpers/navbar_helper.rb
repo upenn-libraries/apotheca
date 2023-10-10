ec1fd698f476c88c3acc5951a2abc4f6d3de3462
Patrick Perkins <lilblizzard97@gmail.com>
2023-10-10 14:11:13 -0400

    remove helper, it wasn't helpful enough

--- rails_app/app/helpers/navbar_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# add active class to a navbar link if the request begins with the correct path
-module NavbarHelper
-  def nav_active(request, path)
-    'active' if request.path.starts_with?(path)
-  end
-end
-
