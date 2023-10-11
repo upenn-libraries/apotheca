56ff42102927fb77e35074285e496ef8ad4a9133
Patrick Perkins <lilblizzard97@gmail.com>
2023-10-11 14:19:28 -0400

    remove synchronous option for deleting assets

--- rails_app/app/transactions/delete_item.rb
+++ rails_app/app/transactions/delete_item.rb
@@ -11,17 +11,10 @@ class DeleteItem
   # An Item can have lots of Assets, so we enqueue jobs to delete each of them here.
   # Note that Sidekiq Jobs cannot receive a Valkyrie::ID object, so we convert to string here.
   # @param [ItemResource] resource
-  # @param [TrueClass, FalseClass] async
-  def delete_assets(resource:, async: true)
-    asset_ids = resource.asset_ids&.map { |id| [id.to_s] }
-    return if asset_ids.blank?
+  def delete_assets(resource:)
+    asset_params = resource.asset_ids&.map { |id| [id.to_s] }
+    return if asset_params.blank?
 
-    if async
-      RemoveAssetJob.perform_bulk(asset_ids)
-    else
-      asset_ids.each do |id|
-        RemoveAssetJob.perform_inline(id.first)
-      end
-    end
+    RemoveAssetJob.perform_bulk(asset_params)
   end
 end
