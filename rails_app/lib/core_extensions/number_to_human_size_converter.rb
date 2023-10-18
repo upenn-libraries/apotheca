ece4262d24e2580e4180b3ab840576d4f65b5111
Patrick Perkins <lilblizzard97@gmail.com>
2023-10-18 15:12:30 -0400

    update comment

--- rails_app/lib/core_extensions/number_to_human_size_converter.rb
+++ rails_app/lib/core_extensions/number_to_human_size_converter.rb
@@ -5,10 +5,9 @@ module ActiveSupport
     class NumberToHumanSizeConverter < NumberConverter
       private
 
-      # We need this monkey patch because Rails has dropped support for "si"
-      # prefixed sizes. Hard drive manufacturers calculate bytes -> to kb by
-      # dividing by 1000 not 1024. In order for us to use this helper in that
-      # context we need the ability to change this base.
+      # We need this monkey patch because Rails has dropped support for "si" prefixed sizes. When displaying file
+      # sizes, we want to calculate bytes -> to kb by dividing by 1000 not 1024. In order for us to use this helper
+      # in that context we need the ability to change this base.
       def base
         options[:base] || 1024
       end
