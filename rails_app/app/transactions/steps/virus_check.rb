cd72ade355f86aa00fc66a24617be6204a0a9881
Mike Kanning <mkanning@upenn.edu>
2023-10-30 14:34:02 -0400

    flesh out VirusCheck step. add specs for added step. update locale file for virus scan preservation event messages.

--- rails_app/app/transactions/steps/virus_check.rb
+++ rails_app/app/transactions/steps/virus_check.rb
@@ -1,41 +1,56 @@
 # frozen_string_literal: true
 
 module Steps
+  # Step to scan a given file for viruses using Clamby/Clamscan
   class VirusCheck
     include Dry::Monads[:result]
 
+    # Perform a virus scan using Clamby. There are 4 potential outcomes:
+    # 1. File is too big and can't be scanned by Clam.
+    # 2. File is scanned and no virus found.
+    # 3. File is scanned and a virus is found. File is deleted from the local system and failure is returned.
+    # 4. Problem using Clamby. Failure is returned and Honeybadger notification is sent.
     def call(**attributes)
       file = attributes[:file] || attributes['file']
-      binding.b
-      if file.present? && file.size <= 2.gigabytes
-
-        result = scan_for_viruses(file.path)
-        if result
-          success = AssetResource::PreservationEvent.virus_check outcome: Premis::Outcomes::SUCCESS.uri,
-                                                                 note: I18n.t('preservation_events.virus_check.success.note'),
-                                                                 implementer: attributes[:updated_by]
-          attributes[:temporary_events] = [success]
+      if skip_scan?(file)
+        attributes[:temporary_events] = AssetResource::PreservationEvent.virus_check(
+          outcome: Premis::Outcomes::SUCCESS.uri, note: I18n.t('preservation_events.virus_check.unscanned'),
+          implementer: attributes[:updated_by]
+        )
+        Success(attributes)
+      else
+        case Clamby.safe?(file.path)
+        when TrueClass
+          attributes[:temporary_events] = AssetResource::PreservationEvent.virus_check(
+            outcome: Premis::Outcomes::SUCCESS.uri, implementer: attributes[:updated_by],
+            note: I18n.t('preservation_events.virus_check.clean')
+          )
           Success(attributes)
-        else
-          failure = AssetResource::PreservationEvent.virus_check outcome: Premis::Outcomes::FAILURE.uri,
-                                                                 note: I18n.t('preservation_events.virus_check.failure.note'),
-                                                                 implementer: attributes[:updated_by]
-          attributes[:temporary_events] = [failure]
+        when FalseClass
+          File.delete(file.path)
           Failure(error: :virus_detected)
+        else
+          report_clamscan_problem(file.path)
+          Failure(error: :clamscan_problem)
         end
       end
     end
 
     private
 
-    def scan_for_viruses(path)
-      scan_result = Clamby.safe?(path)
-      if scan_result
-        true
-      else
-        File.delete(path)
-        false
-      end
+    # Skip the scan if no file is present or if it is over 2 GB
+    # @param [String] file
+    # @return [Boolean]
+    def skip_scan?(file)
+      return true if file.blank?
+
+      file.present? && file.size >= 2.gigabytes
+    end
+
+    # Clamby calls return nil if there is a problem finding the file or using clamscan
+    # @param [String] path
+    def report_clamscan_problem(path)
+      Honeybadger.notify("Problem scanning file (#{path}) using clamscan.")
     end
   end
 end
---
+++ rails_app/spec/transactions/steps/virus_check_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+describe Steps::VirusCheck do
+  describe '#call' do
+    let(:step) { described_class.new }
+    let(:result) { step.call(file: Tempfile.new, updated_by: 'test@test.edu') }
+
+    before do
+      allow(Clamby).to receive(:safe?).with(anything).and_return(clamby_outcome)
+      allow(step).to receive(:skip_scan?).with(anything).and_return(skip_scan)
+    end
+
+    context 'when the file is too large' do
+      let(:skip_scan) { true }
+      let(:clamby_outcome) { nil }
+
+      it 'returns success and sets a preservation message' do
+        expect(result.success?).to be true
+      end
+    end
+
+    context 'when there is a virus' do
+      let(:skip_scan) { false }
+      let(:clamby_outcome) { false }
+
+      it 'fails' do
+        expect(result.failure?).to be true
+      end
+    end
+
+    context 'when there is no virus' do
+      let(:skip_scan) { true }
+      let(:clamby_outcome) { true }
+
+      it 'returns success and sets a preservation message' do
+        expect(result.success?).to be true
+      end
+    end
+
+    context 'when there is a clamscan issue' do
+      let(:skip_scan) { false }
+      let(:clamby_outcome) { nil }
+
+      it 'fails' do
+        expect(result.failure?).to be true
+      end
+    end
+  end
+end
