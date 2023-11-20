940bf09dd1e376b3416f6a04c064e0992b16e5e8
Amrey Mathurin <mamrey@upenn.edu>
2023-11-20 08:38:30 -0500

    Nest params that are not attributes of BulkImport within bulk_import field

--- rails_app/app/controllers/bulk_imports_controller.rb
+++ rails_app/app/controllers/bulk_imports_controller.rb
@@ -94,7 +94,7 @@ class BulkImportsController < ApplicationController
 
   # @return [Array<ActionDispatch::Http::UploadedFile>]
   def asset_spreadsheets
-    params[:spreadsheet_metadata] || []
+    params[:bulk_import][:spreadsheet_metadata] || []
   end
 
   # @return [Array<String>]
