23e4b4c723e80a163069c986908ebc7f39921634
Amrey Mathurin <mamrey@upenn.edu>
2023-11-20 09:14:39 -0500

    Inject top level argument to AssetSet to pass in class that normalizes data

--- rails_app/app/services/import_service/asset_set.rb
+++ rails_app/app/services/import_service/asset_set.rb
@@ -12,12 +12,11 @@ module ImportService
     #   1. { arranged: [ { filename: 'file1.tif', label: '1' }, { filename: 'file2.tif', label: '2' }],
     #        unarranged: [ { filename: 'reference_shot.tif' }] }
     #   2. { arranged_filenames: 'file1.tif;file2.tif', unarranged_filenames: 'reference_shot.tif' }
-    #   3. { structural: [{ filename: 'file1.tif', label: '1' }, { filename: 'file2.tif', label: '2'}] }
     #
     # File locations can be provided via :storage and :path keys:
     #   { storage: 'sceti-completed-n', path: 'object_3' }
-    def initialize(**args)
-      @data = args.deep_symbolize_keys.compact_blank # Store raw asset data provided.
+    def initialize(normalizer: ImportService::AssetSpreadsheetNormalizer, **args)
+      @data = normalizer.process(args.deep_symbolize_keys.compact_blank) # Store raw asset data provided.
       @file_locations = args.key?(:path) || args.key?(:storage) ? FileLocations.new(**args) : nil
 
       @errors = []
@@ -30,20 +29,12 @@ module ImportService
         @errors << 'arranged_filenames/unarranged_filenames cannot be used in conjunction with arranged/unarranged keys'
       end
 
-      # arranged_filename/unarranged_filename or arranged/unarranged keys can't be present when using asset spreadsheet
-      if data[:spreadsheet] && (asset_fields_with_metadata? || asset_fields_without_metadata?)
-        @errors << '(arranged/unarranged)_filename or arranged/unarranged keys used alongside asset spreadsheet'
-      end
-
       # Ensure all arranged/unarranged assets have a filename
       @errors << 'arranged assets missing filename(s)'   if data[:arranged] && !filenames_present?(data[:arranged])
       @errors << 'unarranged assets missing filename(s)' if data[:unarranged] && !filenames_present?(data[:unarranged])
 
-      # Ensure every row of asset spreadsheet has a filename
-      @errors << 'asset filename(s) missing' if data[:spreadsheet] && !filenames_present?(data[:spreadsheet])
-
       # Ensure at least one asset is defined
-      unless %i[arranged_filenames unarranged_filenames arranged unarragned spreadsheet].any? { |k| data.key?(k) }
+      unless %i[arranged_filenames unarranged_filenames arranged unarragned].any? { |k| data.key?(k) }
         @errors << 'no assets defined'
       end
 
@@ -94,8 +85,6 @@ module ImportService
         filenames.blank? ? [] : filenames.split(';').map(&:strip).map { |f| asset_data_object(filename: f) }
       elsif data.key?(type.to_sym)
         data[type.to_sym].map { |a| asset_data_object(**a) }
-      elsif data.key?(:spreadsheet)
-        asset_data_from_spreadsheet(type)
       else
         []
       end
@@ -108,30 +97,6 @@ module ImportService
         file_location: file_locations&.file_location_for(metadata[:filename]),
         **metadata
       )
-    end
-
-    # @return [Array<ImportService::AssetData>]
-    def asset_data_from_spreadsheet(type)
-      sequence_criteria = {
-        arranged: ->(a) { a[:sequence].present? },
-        unarranged: ->(a) { a[:sequence].blank? }
-      }
-
-      selected_data = data[:spreadsheet].select(&sequence_criteria[type.to_sym])
-
-      sorted_data = type == :arranged ? selected_data.sort_by { |a| a[:sequence].to_i } : selected_data
-
-      sorted_data.map { |a| asset_data_object(**a.except(:sequence)) }
-    end
-
-    # @return [Boolean]
-    def asset_fields_with_metadata?
-      data[:arranged] || data[:unarranged]
-    end
-
-    # @return [Boolean]
-    def asset_fields_without_metadata?
-      data[:arranged_filenames] || data[:unarranged_filenames]
     end
   end
 end
