f40259c390629770952964b534e7684e28c08cbb
Mike Kanning <mkanning@upenn.edu>
2023-12-12 01:01:34 +0000

    Improving documentation.

--- rails_app/app/transactions/steps/detach_from_item.rb
+++ rails_app/app/transactions/steps/detach_from_item.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Steps
-  # Deletes preservation, preservation backup and derivative files
+  # Removes linkage of given Asset resource from parent Item
   class DetachFromItem
     include Dry::Monads[:result]
 
