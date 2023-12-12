b09d3028ad57d035c3356ce2e375a36528483bed
Carla Galarza <cgalarza@upenn.edu>
2023-12-12 12:08:03 -0500

    Using unnamed splat operator to pass down additional keyword parameters.

--- rails_app/app/transactions/steps/detach_from_item.rb
+++ rails_app/app/transactions/steps/detach_from_item.rb
@@ -5,14 +5,14 @@ module Steps
   class DetachFromItem
     include Dry::Monads[:result]
 
-    def call(resource:, item:, deleted_by:, **arguments)
+    def call(resource:, item:, deleted_by:, **)
       # Only detach if item is present.
       if item
-        result = DetachAsset.new.call(id: item.id, asset_id: resource.id, updated_by: deleted_by, **arguments)
+        result = DetachAsset.new.call(id: item.id, asset_id: resource.id, updated_by: deleted_by, **)
         return result if result.failure?
       end
 
-      Success(resource: resource, deleted_by: deleted_by, **arguments)
+      Success(resource: resource, deleted_by: deleted_by, **)
     end
   end
 end
