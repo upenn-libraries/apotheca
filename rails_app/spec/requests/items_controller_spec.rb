0c5dcf9ee8286a5a15844ccc791367bcbd93bf85
Patrick Perkins <lilblizzard97@gmail.com>
2023-12-13 14:23:23 -0500

    basic item request specs

--- rails_app/spec/requests/items_controller_spec.rb
+++ rails_app/spec/requests/items_controller_spec.rb
@@ -1,29 +1,95 @@
 # frozen_string_literal: true
 
 describe 'Items Requests' do
-  it 'redirects unauthenticated users to sign in' do
-    get '/resources/items'
-    expect(response).to redirect_to(root_path)
-    expect(flash['alert']).to include 'You need to sign in'
-  end
+  before { sign_in create(:user, user_role) }
 
-  context 'when editing' do
-    let(:item) { persist(:item_resource) }
+  # POST /resources/items
+  context 'when creating an Item' do
+    include_context 'with successful requests to mint EZID'
 
-    context 'without edit role' do
-      it 'redirects viewer users to authenticated root path with authorization message' do
-        sign_in create(:user, :viewer)
-        get edit_item_path(item)
-        expect(response).to redirect_to(authenticated_root_path)
-        expect(flash['alert']).to include 'not authorized'
+    let(:user_role) { :editor }
+    let(:item) { build(:item_resource) }
+
+    context 'with a successful request' do
+      before do
+        post items_path, params: { item: { human_readable_name: item.human_readable_name,
+                                           created_by: item.created_by,
+                                           descriptive_metadata: {
+                                             title: [{ value: item.descriptive_metadata.title.first.value }]
+                                           } } }
+      end
+
+      it 'displays successful alert' do
+        follow_redirect!
+        expect(response.body).to include('Successfully created item.')
+      end
+
+      it 'creates the Item' do
+        expect(
+          Valkyrie::MetadataAdapter.find(:postgres).query_service.find_all_of_model(model: ItemResource).count
+        ).to be 1
       end
     end
+  end
 
-    context 'with proper role' do
-      it 'shows item edit form' do
-        sign_in create(:user, :editor)
-        get edit_item_path(item)
+  # PATCH /resources/items/:id
+  context 'when editing an Item' do
+    let(:user_role) { :editor }
+    let(:item) { persist(:item_resource) }
+
+    context 'with a successful request' do
+      before { patch item_path(item), params: { item: { human_readable_name: 'The new name!' } } }
+
+      it 'displays successful alert' do
+        follow_redirect!
+        expect(response.body).to include('Successfully updated item.')
+      end
+
+      it 'updates the Item' do
+        expect(
+          Valkyrie::MetadataAdapter.find(:postgres).query_service.find_by(id: item.id).human_readable_name
+        ).to eq('The new name!')
+      end
+    end
+  end
+
+  # DELETE /resources/items/:id
+  context 'when deleting an Item' do
+    let(:user_role) { :admin }
+    let(:item) { persist(:item_resource) }
+
+    context 'with a successful request' do
+      before { delete item_path(item) }
+
+      it 'displays successful alert' do
+        follow_redirect!
+        expect(response.body).to include('Successfully deleted Item')
+      end
+
+      it 'deletes the Item' do
+        expect(
+          Valkyrie::MetadataAdapter.find(:postgres).query_service.find_all_of_model(model: ItemResource).count
+        ).to be 0
+      end
+    end
+  end
+
+  # POST /resources/items/:id/refresh_ils_metadata
+  context 'when refreshing ILS metadata' do
+    let(:user_role) { :admin }
+    let(:item) { persist(:item_resource) }
+
+    context 'with a successful request' do
+      before { post refresh_ils_metadata_item_path(item) }
+
+      it 'displays job enqueued alert' do
+        follow_redirect!
         expect(response).to have_http_status :ok
+        expect(response.body).to include('Job to refresh ILS metadata enqueued')
+      end
+
+      it 'enqueues job' do
+        expect(RefreshIlsMetadataJob).to have_enqueued_sidekiq_job.with(item.id)
       end
     end
   end
