91698e02e2b5f0eab6aac59f1d8b0f8d1ed59671
Carla Galarza <cgalarza@upenn.edu>
2023-12-22 10:44:26 -0500

    Dynamically generating methods to check for type of derivative. Refactoring some related code.

--- rails_app/app/resources/derivative_resource.rb
+++ rails_app/app/resources/derivative_resource.rb
@@ -8,15 +8,10 @@ class DerivativeResource < Valkyrie::Resource
   attribute :file_id, Valkyrie::Types::ID
   attribute :stale, Valkyrie::Types::Strict::Bool
 
-  def method_missing(symbol, *_args)
-    raise NoMethodError unless respond_to_missing? symbol
-
-    type == symbol.to_s[...-1]
-  end
-
-  def respond_to_missing?(symbol, _include_private = false)
-    return false unless symbol.to_s[...-1].in? AssetChangeSet::AssetDerivativeChangeSet::TYPES
-
-    symbol.to_s.end_with? '?'
+  # Methods to check what type of derivative this is.
+  (AssetChangeSet::AssetDerivativeChangeSet::TYPES + ItemChangeSet::ItemDerivativeChangeSet::TYPES).each do |symbol|
+    define_method "#{symbol}?" do
+      symbol == type
+    end
   end
 end
