cbab98f8df7f36873179231566faf35e15536d13
Carla Galarza <cgalarza@upenn.edu>
2024-02-21 13:00:41 -0500

    Creating new syntax highlighter component that can be used as the body for the technical metadata modal.

--- rails_app/app/components/modal/component.rb
+++ rails_app/app/components/modal/component.rb
@@ -3,19 +3,11 @@
 module Modal
   # Modal and button component
   class Component < ViewComponent::Base
-    def initialize(value:, body:, button_variant: :primary, **options)
-      @id = "modal_component_#{object_id}"
-      @value = value
-      @body = body
-      @button_class = Array.wrap(@button_class).push('btn', "btn-#{button_variant}")
-      @modal_class = configure_modal(options).join(' ')
-      @preview = options[:preview]
-    end
-
-    def highlight(text, language)
-      formatter = Rouge::Formatters::HTML.new
-      lexer = Rouge::Lexer.find(language)
-      formatter.format(lexer.lex(text))
+    def initialize(title:, id:, button_variant: :primary, **options)
+      @id = id
+      @title = title
+      @button_classes = ['btn', "btn-#{button_variant}"]
+      @modal_classes = configure_modal(options)
     end
 
     private
