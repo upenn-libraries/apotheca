ae7eb32cac60919d3a32a4bf3507b3db796bd81b
Amrey Mathurin <mamrey@upenn.edu>
2024-09-05 12:32:19 -0400

    Map only MARC 655 $a to physical_format metadata field

--- rails_app/app/services/metadata_extractor/marmite/transformer/default_mapping_rules.rb
+++ rails_app/app/services/metadata_extractor/marmite/transformer/default_mapping_rules.rb
@@ -95,7 +95,7 @@ module MetadataExtractor
         map_datafield '651', to: :geographic_subject, value: { subfields: A_TO_Z, join: ' -- ' },
                              uri: { subfields: '0' }
         map_datafield '651', to: :coverage, value: { subfields: 'y' }
-        map_datafield '655', to: :physical_format, value: { subfields: A_TO_Z, join: ' -- ' }, uri: { subfields: '0' }
+        map_datafield '655', to: :physical_format, value: { subfields: 'a' }, uri: { subfields: '0' }
         map_datafield '700', to: :name, value: { subfields: %w[a b c d], join: SPACE }, uri: { subfields: '0' },
                              custom: method(:add_role_to_name)
         map_datafield '710', to: :name, value: { subfields: %w[a b d], join: SPACE }, uri: { subfields: '0' },
