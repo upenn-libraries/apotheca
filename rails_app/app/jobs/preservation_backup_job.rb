0962980f22801e8f1239107539cb12ec3a9579a0
Patrick Perkins <lilblizzard97@gmail.com>
2024-09-25 14:24:41 -0400

    return success for preservation backup transaction if asset is not found

--- rails_app/app/jobs/preservation_backup_job.rb
+++ rails_app/app/jobs/preservation_backup_job.rb
@@ -5,6 +5,11 @@ class PreservationBackupJob < TransactionJob
   sidekiq_options queue: :low
 
   def transaction(asset_id, updated_by)
-    PreservationBackup.new.call(id: asset_id, updated_by: updated_by)
+    result = PreservationBackup.new.call(id: asset_id, updated_by: updated_by)
+    # We return a success here if the asset resource is not found to avoid retrying the job,
+    # since the asset will never be found
+    return Dry::Monads::Success("Asset #{asset_id} not found.") if result.failure? && result.failure[:error] == :resource_not_found
+
+    result
   end
 end
