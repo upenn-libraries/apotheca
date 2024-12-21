deed093040cb1b73b96b4a4a842cc42e7b778a2d
Doug Emery <emeryr@upenn.edu>
2024-12-20 20:19:44 -0500

    Job to bulk enqueue ILS metadata refresh jobs

---
+++ rails_app/app/jobs/enqueue_bulk_refresh_ils_metadata_job.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# Enqueue Bulk Refresh ILS Metadata Job for all ItemResources with bibnumbers
+class EnqueueBulkRefreshIlsMetadataJob
+  include Sidekiq::Job
+
+  sidekiq_options queue: :low
+
+  def perform
+    args = bulk_args.compact_blank.to_a
+    RefreshIlsMetadataJob.perform_bulk(args)
+  end
+
+  private
+
+  # Retrieve lazily evaluated collection of iterm args for **all** items
+  # with a bibnumber
+  #
+  # @return [Enumerator::Lazy]
+  def bulk_args
+    query_service.custom_queries
+                 .items_with_bibnumber
+                 .map { |asset| [asset.id.to_s, Settings.system_user] }
+  end
+
+  def query_service
+    @query_service ||= Valkyrie::MetadataAdapter.find(:postgres).query_service
+  end
+end
