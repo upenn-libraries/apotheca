c6b4b5f41551018667ece67405058244eebe1498
Doug Emery <emeryr@upenn.edu>
2025-01-16 13:37:06 -0500

    Make ActionsTable Actions header optional

--- rails_app/app/components/actions_table/component.rb
+++ rails_app/app/components/actions_table/component.rb
@@ -4,5 +4,9 @@ module ActionsTable
   # Renders table of actions for a resource
   class Component < ViewComponent::Base
     renders_many :actions, Action::Component
+
+    def initialize(header: true)
+      @header = header
+    end
   end
 end
