30d7f47d55fc9c2bc5aa71e336f3f2f779a7d705
Carla Galarza <cgalarza@upenn.edu>
2025-01-27 16:41:35 -0500

    Refactor PDFGenerator to add AssetWrapper class to encapsulate asset logic needed.

--- rails_app/app/services/derivative_service/item/pdf_generator.rb
+++ rails_app/app/services/derivative_service/item/pdf_generator.rb
@@ -25,35 +25,19 @@ module DerivativeService
       def pdf
         return unless pdfable?
 
-        # Run through all the assets and generate jpg derivatives for each one
-        assets_data = item.arranged_assets.map do |asset|
-          # DPI is required in order to size the PDF correctly.
-          original_dpi = asset.technical_metadata.dpi
-          raise "Cannot generate PDF without DPI: #{asset.id} is missing dpi." unless original_dpi
-
-          desired_dpi = original_dpi/2 # Shrink images in half
-
-          {
-            jpg: { file: asset_jpg(asset, desired_dpi), dpi: desired_dpi },
-            textonly_pdf: { file: asset_textonly_pdf(asset) },
-            label: asset.label,
-            bookmarks: asset.annotations.map(&:text)
-          }
-        end
+        # Wrap all assets with AssetWrapper class.
+        assets = item.arranged_assets.map { |a| AssetWrapper.new(a) }
 
         # Create PDF.
-        pdf = create_pdf(assets_data)
+        pdf = create_pdf(assets)
 
         # Create PDF derivative file.
         pdf_derivative = DerivativeFile.new mime_type: 'application/pdf', extension: '.pdf'
         pdf.write(pdf_derivative.path, optimize: true)
 
-        # Delete temporary derivative files.
+        # Cleanup temporary derivative files.
         # These need to be deleted after the PDF file is written because otherwise they are still being referenced.
-        assets_data.each do |asset|
-          asset[:jpg][:file].cleanup!
-          asset[:textonly_pdf][:file]&.close
-        end
+        assets.each(&:cleanup!)
 
         # Return PDF derivative file.
         pdf_derivative
@@ -67,60 +51,30 @@ module DerivativeService
 
       private
 
-      # Create JPEG derivative for the asset given resized to the desired dpi.
-      #
-      # @param [AssetResource] asset
-      # @param [Integer] desired_dpi of derivative jpg
-      # @return [DerivativeFile]
-      def asset_jpg(asset, desired_dpi)
-        # Read in access derivative.
-        file = Valkyrie::StorageAdapter.find_by(id: asset.access.file_id)
-
-        # Scale the image should be resized by.
-        scale = desired_dpi.to_f / asset.technical_metadata.dpi.to_f
-
-        # Create JPEG image
-        image = Vips::Image.new_from_buffer(file.read, '')
-        image = image.autorot.thumbnail_image((asset.technical_metadata.width * scale).to_i, height: (asset.technical_metadata.height.to_f * scale).to_i)
-        image_derivative = DerivativeFile.new mime_type: 'image/jpeg'
-        image.jpegsave(image_derivative.path, strip: true)
-
-        image_derivative
-      end
-
-      # Return text-only PDF (containing OCR), if one is present for the asset.
-      #
-      # @return [Valkyrie::StorageAdapter::StreamFile]
-      def asset_textonly_pdf(asset)
-        return if asset.textonly_pdf.blank?
-
-        Valkyrie::StorageAdapter.find_by(id: asset.textonly_pdf.file_id)
-      end
-
       # Create Item PDF including OCR (if available), bookmarks and cover page.
       #
       # @todo: Need to create cover page.
       #
-      # @param [Hash] assets_data # TODO:
+      # @param [Array<AssetWrapper>] assets # TODO:
       # @return [HexaPDF::Document]
-      def create_pdf(assets_data)
+      def create_pdf(assets)
         doc = HexaPDF::Document.new
 
-        assets_data.each do |data|
-          image = doc.images.add(data[:jpg][:file].path)
+        assets.each do |asset|
+          image = doc.images.add(asset.image.path)
 
-          iw = image.width.to_f * (72.0 / data[:jpg][:dpi].to_f)
-          ih = image.height.to_f * (72.0 / data[:jpg][:dpi].to_f)
+          iw = image.width.to_f * (72.0 / asset.image_dpi)
+          ih = image.height.to_f * (72.0 / asset.image_dpi)
 
           page = doc.pages.add([0, 0, iw, ih])
 
           page.canvas.image(image, at: [0, 0], width: iw, height: ih)
 
           # Overlay OCR text layer, if present
-          if (textonly_pdf = data[:textonly_pdf][:file])
+          if (textonly_pdf = asset.textonly_pdf)
             ocr_text = HexaPDF::Document.open(textonly_pdf.disk_path)
             ocr_overlay = doc.import(ocr_text.pages.first.to_form_xobject)
-            page.canvas.xobject(ocr_overlay, at: [0, 0], width: iw, height: ih)
+            page.canvas(type: :overlay).xobject(ocr_overlay, at: [0, 0], width: iw, height: ih)
           end
 
           # Add bookmarks
---
+++ rails_app/app/services/derivative_service/item/pdf_generator/asset_wrapper.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+module DerivativeService
+  module Item
+    class PDFGenerator
+      # Wrapper for AssetResource that provides additional logic needed for creating a PDF representation of an Item.
+      class AssetWrapper
+        attr_reader :asset
+
+        DEFAULT_SCALE = 0.5 # Default scale applied to images
+
+        def initialize(asset)
+          @asset = asset
+        end
+
+        # Calculates the DPI of the scaled derivative image.
+        def image_dpi
+          asset.technical_metadata.dpi * DEFAULT_SCALE
+        end
+
+        # Return scaled asset JPEG derivative.
+        #
+        # @return [DerivativeFile]
+        def image
+          @image ||= create_jpg
+        end
+
+        # Return text-only PDF (containing OCR), if one is present for the asset.
+        #
+        # @return [Valkyrie::StorageAdapter::StreamFile]
+        def textonly_pdf
+          @textonly_pdf ||= load_textonly_pdf
+        end
+
+        # Cleans up any opened files.
+        def cleanup!
+          image&.cleanup!
+          textonly_pdf&.close
+
+          @image = nil
+          @textonly_pdf = nil
+        end
+
+        private
+
+        # Create asset JPEG derivative.
+        #
+        # @return [DerivativeFile]
+        def create_jpg
+          # Read in access derivative.
+          file = Valkyrie::StorageAdapter.find_by(id: asset.access.file_id)
+
+          # Create JPEG image
+          image = Vips::Image.new_from_buffer(file.read, '')
+          image = image.autorot.thumbnail_image(asset.technical_metadata.width * DEFAULT_SCALE,
+                                                height: asset.technical_metadata.height * DEFAULT_SCALE)
+          image_derivative = DerivativeFile.new mime_type: 'image/jpeg'
+          image.jpegsave(image_derivative.path, strip: true)
+
+          image_derivative
+        end
+
+        # Load text-only PDF (containing OCR), if one is present for the asset.
+        #
+        # @return [Valkyrie::StorageAdapter::StreamFile]
+        def load_textonly_pdf
+          return if asset.textonly_pdf.blank?
+
+          Valkyrie::StorageAdapter.find_by(id: asset.textonly_pdf.file_id)
+        end
+      end
+    end
+  end
+end
