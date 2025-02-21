b64bf27ba47627fac7907fc74d9ba23ebbe75433
Patrick Perkins <pperkins@upenn.edu>
2025-02-21 12:35:06 -0500

    rename report_json to file

--- rails_app/app/models/report.rb
+++ rails_app/app/models/report.rb
@@ -4,8 +4,8 @@
 class Report < ApplicationRecord
   include Queueable
 
-  has_one_attached :report_json
-  validates :generated_at, presence: true, if: -> { report_json.attached? }
+  has_one_attached :file
+  validates :generated_at, presence: true, if: -> { file.attached? }
   # TODO: validate report_type is in a constant REPORT_TYPES array
   # TODO: more validations potentially?
   # bulk_export validate state in the model, but that validation is already included in Queueable
@@ -14,7 +14,7 @@ class Report < ApplicationRecord
     # TODO: implement run method when repository growth report details are finalized
     #
     # this method will likely call a service that generates the report given all item data
-    # and send that data to the `report_json` file (i think...)
+    # and send that data to the file (i think...)
     # maybe something like `result = ReportService.build(...)`
     #
     # call `success!` if everything succeeds, rescue errors and call `failure!`
