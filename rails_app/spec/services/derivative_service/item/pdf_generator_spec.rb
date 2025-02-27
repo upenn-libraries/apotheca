64c1091bc2e6c6d2ffc525986d4846aa66b0fa59
Amrey Mathurin <mamrey@upenn.edu>
2025-02-27 22:17:38 +0000

    Clarifying test context.

--- rails_app/spec/services/derivative_service/item/pdf_generator_spec.rb
+++ rails_app/spec/services/derivative_service/item/pdf_generator_spec.rb
@@ -42,7 +42,7 @@ describe DerivativeService::Item::PDFGenerator do
   describe '#pdfable?' do
     let(:generator) { described_class.new(item) }
 
-    context 'when contains arranged image assets' do
+    context 'when item contains arranged image assets' do
       let(:item) { persist(:item_resource, :with_full_assets_all_arranged) }
 
       it 'returns true' do
@@ -50,7 +50,7 @@ describe DerivativeService::Item::PDFGenerator do
       end
     end
 
-    context 'when does not contain any arranged assets' do
+    context 'when item does not contain any arranged assets' do
       let(:item) { persist(:item_resource) }
 
       it 'returns false' do
@@ -58,7 +58,7 @@ describe DerivativeService::Item::PDFGenerator do
       end
     end
 
-    context 'when contains a non-image arranged asset' do
+    context 'when item contains a non-image arranged asset' do
       let(:item) do
         persist(:item_resource, :with_full_assets_all_arranged, asset1: persist(:asset_resource, :with_pdf_file))
       end
@@ -68,7 +68,7 @@ describe DerivativeService::Item::PDFGenerator do
       end
     end
 
-    context 'when contains more than 2000 arranged assets' do
+    context 'when item contains more than 2000 arranged assets' do
       let(:item) { persist(:item_resource, structural_metadata: { arranged_assets_ids: (1...2002).to_a }) }
 
       it 'returns false' do
