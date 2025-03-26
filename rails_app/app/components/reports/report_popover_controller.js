ffbdb30fbfc77690f0094c86c9d93b3d2711e058
Patrick Perkins <pperkins@upenn.edu>
2025-03-26 16:47:04 -0400

    format JS

--- rails_app/app/components/reports/report_popover_controller.js
+++ rails_app/app/components/reports/report_popover_controller.js
@@ -3,6 +3,6 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["popover"];
   connect() {
-		this.popoverTargets.map(target => new bootstrap.Popover(target));
+    this.popoverTargets.map((target) => new bootstrap.Popover(target));
   }
 }
