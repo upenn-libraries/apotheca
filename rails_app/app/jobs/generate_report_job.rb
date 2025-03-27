f6ca83b5e2c0e4dacf93923fb0035b381c9993cf
Patrick Perkins <pperkins@upenn.edu>
2025-03-27 15:41:25 -0400

    remove default report_type arg in job

--- rails_app/app/jobs/generate_report_job.rb
+++ rails_app/app/jobs/generate_report_job.rb
@@ -6,7 +6,7 @@ class GenerateReportJob
   sidekiq_options queue: :medium
 
   # Default to repository growth report
-  def perform(report_type = 'repository_growth')
+  def perform(report_type)
     report = Report.create!(report_type: report_type)
     report.process!
   end
