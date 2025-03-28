26c33a5376c38e447cecfb61b6af5beb8bde1bd2
Carla Galarza <cgalarza@upenn.edu>
2025-03-28 20:51:37 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Carla Galarza <cgalarza@upenn.edu>

--- rails_app/app/jobs/generate_report_job.rb
+++ rails_app/app/jobs/generate_report_job.rb
@@ -5,7 +5,6 @@ class GenerateReportJob
   include Sidekiq::Job
   sidekiq_options queue: :medium
 
-  # Default to repository growth report
   def perform(report_type)
     report = Report.create!(report_type: report_type)
     report.process!
