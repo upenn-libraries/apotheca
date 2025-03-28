4f2b33ddbd677d62e4fe50e807776b6938261a5e
Patrick Perkins <pperkins@upenn.edu>
2025-03-28 13:28:48 -0400

    add link style for red links

--- rails_app/app/components/popover/component.rb
+++ rails_app/app/components/popover/component.rb
@@ -3,16 +3,17 @@
 module Popover
   # Popover component
   class Component < ViewComponent::Base
-    attr_reader :title, :popover_content, :text
+    attr_reader :title, :popover_content, :text, :link_style
 
     # Initializes the popover with the given attributes.
     # @param title [String] The title of the popover.
     # @param popover_content [String] The content of the popover.
     # @param text [String] The text that be used to trigger the popover.
-    def initialize(title:, popover_content:, text:)
+    def initialize(title:, popover_content:, text:, link_style: 'primary')
       @title = title
       @popover_content = popover_content
       @text = text
+      @link_style = link_style
     end
   end
 end
