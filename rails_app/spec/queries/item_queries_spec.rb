a6d91c66da3a78101a7f7ed6cd76bbb967a5d444
Carla Galarza <cgalarza@upenn.edu>
2025-04-04 15:53:39 -0400

    Create rake task to migrate ocr_type to ocr_strategy; Add new custom query for rake task.

---
+++ rails_app/spec/queries/item_queries_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+describe ItemQueries do
+  let(:query) do
+    described_class.new(query_service: Valkyrie::MetadataAdapter.find(:postgres).query_service)
+  end
+
+  describe '#find_by_unique_identifier' do
+    let(:unique_identifier) { 'ark:/99999/fk4vx4' }
+    let(:item) { persist(:item_resource, unique_identifier: unique_identifier) }
+
+    before do
+      item
+      persist(:item_resource)
+    end
+
+    context 'when item with unique_identifier exists' do
+      it 'returns expected item' do
+        expect(query.find_by_unique_identifier(unique_identifier: unique_identifier).id).to eql item.id
+      end
+    end
+
+    context 'when item with unique_identifier is not found' do
+      it 'returns nil' do
+        expect(query.find_by_unique_identifier(unique_identifier: 'ark:/random/random')).to be_nil
+      end
+    end
+  end
+end
