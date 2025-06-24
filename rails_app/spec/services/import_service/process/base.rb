31b43039ffc75b35686aedd332346c095b4215af
Amrey Mathurin <mamrey@upenn.edu>
2025-06-24 12:22:07 -0400

    Add specs for validating bibnumber in ImportService

--- rails_app/spec/services/import_service/process/base.rb
+++ rails_app/spec/services/import_service/process/base.rb
@@ -11,5 +11,25 @@ shared_examples_for 'a ImportService::Process::Base' do
       expect(process.valid?).to be false
       expect(process.errors).to include 'imported_by must always be provided'
     end
+
+    context 'with malformed bibnumber' do
+      it 'adds error' do
+        process = build(:import_process, :create,
+                        metadata: { 'bibnumber' => [{ value: '123456789' }] })
+        expect(process.valid?).to be false
+        expect(process.errors).to include('failed to retrieve marmite metadata: bibnumber must be valid')
+      end
+    end
+
+    context 'when marmite fails to retrieve metadata when provided properly formatted bibnumber' do
+      include_context 'with unsuccessful Marmite request'
+
+      it 'adds error' do
+        process = build(:import_process, :create,
+                        metadata: { 'bibnumber' => [{ value: MMSIDValidator::EXAMPLE_VALID_MMS_ID }] })
+        expect(process.valid?).to be false
+        expect(process.errors).to include('failed to retrieve marmite metadata: bibnumber must be valid')
+      end
+    end
   end
 end
