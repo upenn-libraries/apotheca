2673ed82f169111417ae8f5332d8dc10ad80004e
Amrey Mathurin <mamrey@upenn.edu>
2025-07-08 13:38:54 -0400

    Implement resource api asset file endpoint

--- rails_app/app/controllers/api/resources/assets_controller.rb
+++ rails_app/app/controllers/api/resources/assets_controller.rb
@@ -4,11 +4,22 @@ module API
   module Resources
     # API actions for AssetResources
     class AssetsController < APIController
+      class FileTypeError < InvalidParameterError; end
+
+      FILES = %w[thumbnail iiif_image preservation].freeze
+      IIIF_IMAGE_SIZE = 'max'
+
       before_action :load_asset
       before_action :load_item
       def show; end
 
-      def file; end
+      def file
+        type = params[:file]
+
+        raise FileTypeError, I18n.t('api.exceptions.invalid_param.file_type', type: type) unless type.in? FILES
+
+        redirect_to_file(type)
+      end
 
       private
 
@@ -25,6 +36,22 @@ module API
                                          .first
         raise ResourceNotFound, I18n.t('api.exceptions.not_found') if @item.nil?
         raise NotPublishedError, I18n.t('api.exceptions.not_published') unless @item.published
+      end
+
+      def redirect_to_file(type)
+        case type
+        when 'thumbnail' then redirect_to_presigned_url(@asset.thumbnail.file_id, thumbnail_filename)
+        when 'iiif_image' then redirect_to_iiif_image_server(@asset, IIIF_IMAGE_SIZE)
+        when 'preservation' then redirect_to_presigned_url(@asset.preservation_file_id, @asset.original_filename)
+        else
+          raise FileTypeError, I18n.t('api.exceptions.invalid_param.file_type', type: type)
+        end
+      end
+
+      # @return [String]
+      def thumbnail_filename
+        "#{File.basename(@asset.original_filename,
+                         File.extname(@asset.original_filename))}-thumbnail.#{@asset.thumbnail.extension}"
       end
     end
   end
