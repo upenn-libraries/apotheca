da1320e588408d0e718f6be9a3bd2623cab1122f
Patrick Perkins <pperkins@upenn.edu>
2025-08-13 13:14:17 -0400

    simplify top level spec to test for high level attributes and manifest structure

--- rails_app/spec/services/derivative_service/item/manifest_generator/v3_spec.rb
+++ rails_app/spec/services/derivative_service/item/manifest_generator/v3_spec.rb
@@ -19,154 +19,113 @@ describe DerivativeService::Item::ManifestGenerator::V3 do
         persist(
           :item_resource, :with_full_assets_all_arranged,
           descriptive_metadata: {
-            title: [{ value: 'New Item' }],
-            name: [{ value: 'Random, Person', role: [{ value: 'Illustrator' }, { value: 'Creator' }] }],
-            rights: [{ value: 'In Copyright', uri: 'http://rightsstatements.org/vocab/InC/1.0/' }],
-            rights_note: [{ value: 'Contact copyright owner' }]
+            title: [{ value: 'Test IIIF Manifest Item' }],
+            name: [{ value: 'Test, Author', role: [{ value: 'Creator' }] }]
           }
         )
       end
 
-      it 'includes top level attributes' do
-        expect(json).to include(
-          'id' => ending_with('manifest'),
-          'label' => { 'none' => ['New Item'] },
-          'behavior' => ['individuals'],
-          'viewingDirection' => 'left-to-right',
-          'requiredStatement' => {
-            'label' => { 'none' => ['Attribution'] },
-            'value' => { 'none' => ['Provided by the University of Pennsylvania Libraries.'] }
-          }
+      it 'creates a valid IIIF v3 manifest with correct context' do
+        expect(json['@context']).to include(
+          'http://www.w3.org/ns/anno.jsonld',
+          'http://iiif.io/api/presentation/3/context.json'
         )
       end
 
-      it 'includes manifest metadata' do
-        expect(json['metadata']).to contain_exactly(
-          { 'label' => { 'none' => ['Available Online'] },
-            'value' => { 'none' => [starting_with('https://colenda.library.upenn.edu/catalog/')] } },
-          { 'label' => { 'none' => ['Title'] }, 'value' => { 'none' => ['New Item'] } },
-          { 'label' => { 'none' => ['Name'] }, 'value' => { 'none' => ['Random, Person (Illustrator, Creator)'] } },
-          { 'label' => { 'none' => ['Rights'] },
-            'value' => { 'none' => ['http://rightsstatements.org/vocab/InC/1.0/'] } },
-          { 'label' => { 'none' => ['Rights Note'] }, 'value' => { 'none' => ['Contact copyright owner'] } }
-        )
+      it 'sets manifest type correctly' do
+        expect(json['type']).to eq('Manifest')
       end
 
-      it 'includes item level thumbnail' do
-        expect(json['thumbnail'].first).to include(
-          'id' => starting_with("#{Settings.image_server.url}/iiif/3")
-                     .and(ending_with('/full/!200,200/0/default.jpg')),
-          'type' => 'Image',
-          'format' => 'image/jpeg'
-        )
+      it 'includes manifest ID' do
+        expect(json['id']).to be_present
       end
 
-      it 'includes structures' do
-        expect(json['structures'].first).to include(
-          'id' => ending_with('toc/1'),
-          'label' => { 'none' => ['Front of Card, Front'] },
-          'items' => containing_exactly(
-            a_hash_including(
-              'id' => ending_with('canvas'),
-              'label' => { 'none' => ['Front'] }
-            )
-          )
-        )
+      it 'includes all required top-level properties' do
+        %w[label requiredStatement behavior viewingDirection metadata items].each do |property|
+          expect(json).to have_key(property)
+        end
       end
 
-      it 'includes items' do
-        item = json['items'][0]
-        expect(item).to include(
-          'type' => 'Canvas',
-          'id' => ending_with('canvas'),
-          'label' => { 'none' => ['Front'] }
-        )
+      it 'populates items array with canvases' do
+        expect(json['items']).to be_an(Array).and be_present
       end
 
-      it 'includes rendering for pdf' do
-        expect(json['rendering'][0]).to include(
-          'id' => ending_with('pdf'),
-          'label' => { 'en' => ['Download PDF'] },
-          'type' => 'Text',
-          'format' => 'application/pdf'
-        )
-      end
-
-      it 'includes canvases in items' do
-        canvases = json['items']
-        expect(canvases[0]).to include(
-          'id' => ending_with('canvas'),
-          'label' => { 'none' => ['Front'] },
-          'height' => 238,
-          'width' => 400,
-          'items' => contain_exactly(
-            a_hash_including(
-              'id' => ending_with('annotation-page/1'),
-              'type' => 'AnnotationPage',
-              'items' => contain_exactly(
-                a_hash_including(
-                  'id' => ending_with('annotation/1'),
-                  'type' => 'Annotation',
-                  'motivation' => 'painting',
-                  'body' => a_hash_including(
-                    'id' => starting_with("#{Settings.image_server.url}/iiif/3"),
-                    'height' => 238,
-                    'width' => 400
-                  )
-                )
-              )
-            )
-          ),
-          'rendering' => containing_exactly(
-            a_hash_including(
-              'id' => ending_with('preservation'),
-              'label' => { 'en' => ['Original File - 291 KB'] },
-              'type' => 'Image',
-              'format' => 'image/tiff'
-            )
-          )
-        )
+      it 'includes canvases with correct type in items' do
+        expect(json['items'].first['type']).to eq('Canvas')
       end
     end
 
-    context 'when item contains assets without labels' do
+    context 'when assets have annotations' do
       subject(:json) { JSON.parse(iiif_service.manifest.read) }
 
-      let(:asset) { persist(:asset_resource, :with_image_file, :with_derivatives, :with_metadata_no_label) }
       let(:item) do
         persist(
           :item_resource, :with_full_assets_all_arranged,
-          asset_ids: [asset.id],
-          structural_metadata: { arranged_asset_ids: [asset.id] },
-          descriptive_metadata: { title: [{ value: 'Item Without Asset Labels' }] }
+          descriptive_metadata: { title: [{ value: 'Test Item with Annotations' }] }
         )
       end
 
-      it 'generates a manifest with default page numbering' do
-        expect(json['items'][0]).to include(
-          'id' => ending_with('canvas'),
-          'label' => { 'none' => ['p. 1'] }
+      it 'includes structures property when annotations exist' do
+        expect(json).to have_key('structures')
+      end
+
+      it 'populates structures as array when annotations exist' do
+        expect(json['structures']).to be_an(Array)
+      end
+
+      it 'includes ranges with correct type when annotations exist' do
+        expect(json['structures'].first['type']).to eq('Range')
+      end
+    end
+
+    context 'when item has thumbnail' do
+      subject(:json) { JSON.parse(iiif_service.manifest.read) }
+
+      let(:item) do
+        persist(
+          :item_resource, :with_full_assets_all_arranged,
+          descriptive_metadata: { title: [{ value: 'Test Item with Thumbnail' }] }
         )
       end
 
-      it 'includes basic manifest information' do
-        expect(json).to include(
-          'id' => ending_with('manifest'),
-          'label' => { 'none' => ['Item Without Asset Labels'] }
+      it 'includes thumbnail property when thumbnail exists' do
+        expect(json).to have_key('thumbnail')
+      end
+
+      it 'populates thumbnail as array when thumbnail exists' do
+        expect(json['thumbnail']).to be_an(Array)
+      end
+
+      it 'includes thumbnail with correct type when thumbnail exists' do
+        expect(json['thumbnail'].first['type']).to eq('Image')
+      end
+    end
+
+    context 'when PDF is available' do
+      subject(:json) { JSON.parse(iiif_service.manifest.read) }
+
+      let(:item) do
+        persist(
+          :item_resource, :with_full_assets_all_arranged,
+          descriptive_metadata: { title: [{ value: 'Test Item with PDF' }] }
         )
       end
 
-      it 'includes structures information' do
-        expect(json['structures'].first).to include(
-          'id' => ending_with('toc/1'),
-          'label' => { 'none' => ['Illuminated P'] },
-          'items' => containing_exactly(
-            a_hash_including(
-              'id' => ending_with('canvas'),
-              'label' => { 'none' => [''] }
-            )
-          )
-        )
+      before do
+        pdf_generator_double = instance_double(DerivativeService::Item::PDFGenerator, pdfable?: true)
+        allow(DerivativeService::Item::PDFGenerator).to receive(:new)
+          .with(item)
+          .and_return(pdf_generator_double)
+      end
+
+      it 'includes PDF rendering with correct type' do
+        pdf_rendering = json['rendering']&.find { |r| r['format'] == 'application/pdf' }
+        expect(pdf_rendering['type']).to eq('Text') if pdf_rendering
+      end
+
+      it 'includes PDF rendering with correct label' do
+        pdf_rendering = json['rendering']&.find { |r| r['format'] == 'application/pdf' }
+        expect(pdf_rendering['label']['en']).to eq(['Download PDF']) if pdf_rendering
       end
     end
 
@@ -178,7 +137,8 @@ describe DerivativeService::Item::ManifestGenerator::V3 do
 
       it 'raises an error' do
         expect { iiif_service.manifest }.to raise_error(
-          described_class::MissingDerivative
+          described_class::MissingDerivative,
+          /Derivatives missing for/
         )
       end
     end
@@ -194,25 +154,11 @@ describe DerivativeService::Item::ManifestGenerator::V3 do
       end
     end
 
-    context 'when the iiif_image is not present but the access image derivative is' do
-      subject(:json) { JSON.parse(iiif_service.manifest.read) }
+    context 'when item has no assets' do
+      let(:item) { persist(:item_resource, asset_ids: [], structural_metadata: { arranged_asset_ids: [] }) }
 
-      let(:access_derivative) do
-        asset = persist(:asset_resource, :with_image_file, :with_derivatives)
-        iiif_image = asset.derivatives.find(&:iiif_image?)
-        iiif_image.type = 'access'
-        [iiif_image]
-      end
-      let(:asset) { persist(:asset_resource, :with_image_file, derivatives: access_derivative) }
-
-      let(:item) do
-        persist(:item_resource, asset_ids: [asset.id], thumbnail_asset_id: [asset.id],
-                                structural_metadata: { arranged_asset_ids: [asset.id] })
-      end
-
-      it 'uses the expected identifier in the IIIF image_url' do
-        identifier = CGI.escape(asset.pyramidal_tiff.file_id.to_s.split(Valkyrie::Storage::Shrine::PROTOCOL)[1])
-        expect(json['thumbnail'][0]['service'][0]['id']).to eq("#{Settings.image_server.url}/iiif/3/#{identifier}")
+      it 'returns nil' do
+        expect(iiif_service.manifest).to be_nil
       end
     end
   end
