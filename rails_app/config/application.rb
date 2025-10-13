363955c9959a3cc523fc04509109d013617b0ca6
Carla Galarza <cgalarza@upenn.edu>
2025-10-13 13:43:24 -0400

    Upgrade to Rails 7.2

--- rails_app/config/application.rb
+++ rails_app/config/application.rb
@@ -24,7 +24,7 @@ module Apotheca
   # Application configuration
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
     # Configuration for the application, engines, and railties goes here.
     #
