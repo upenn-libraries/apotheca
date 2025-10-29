1f908f9c269950b2c61d7864168030afdea3bc61
Carla Galarza <cgalarza@upenn.edu>
2025-10-29 13:54:29 -0400

    Moving refresh_all_ils_metadata action to system actions controller because in hindsight its a better place for it and it helps reduce the size of the item controller

--- rails_app/app/controllers/system_actions_controller.rb
+++ rails_app/app/controllers/system_actions_controller.rb
@@ -2,7 +2,15 @@
 
 # System-level actions
 class SystemActionsController < UIController
-  def index
-    authorize! :manage, :system_actions
+  authorize_resource class: false
+
+  def index; end
+
+  def refresh_all_ils_metadata
+    if EnqueueBulkRefreshIlsMetadataJob.perform_async(current_user.email)
+      redirect_to system_actions_path, notice: I18n.t('actions.item.refresh_all_ILS.success')
+    else
+      redirect_to system_actions_path, notice: I18n.t('actions.item.refresh_all_ILS.failure')
+    end
   end
 end
--- rails_app/spec/requests/system_actions_controller_spec.rb
+++ rails_app/spec/requests/system_actions_controller_spec.rb
@@ -1,21 +1,18 @@
 # frozen_string_literal: true
 
 RSpec.describe 'SystemActions requests' do
-  context 'with an unauthenticated user' do
-    it 'redirects user to the root path' do
-      get system_actions_path
-      expect(response).to redirect_to(root_path)
-      expect(flash['alert']).to include 'You need to sign in'
+  # GET /system_actions
+  context 'when viewing system actions listing' do
+    context 'with an unauthenticated user' do
+      it 'redirects user to the root path' do
+        get system_actions_path
+        expect(response).to redirect_to(root_path)
+        expect(flash['alert']).to include 'You need to sign in'
+      end
     end
-  end
 
-  context 'with a logged in user' do
-    before { sign_in user }
-
-    let(:user) { create(:user, role) }
-
-    context 'with a viewer' do
-      let(:role) { :viewer }
+    context 'with an unauthorized user' do
+      before { sign_in create(:user, :editor) }
 
       it 'redirects viewer users to authenticated root path with authorization message' do
         get system_actions_path
@@ -24,22 +21,60 @@ RSpec.describe 'SystemActions requests' do
       end
     end
 
-    context 'with an editor' do
-      let(:role) { :editor }
-
-      it 'redirects viewer users to authenticated root path with authorization message' do
-        get system_actions_path
-        expect(response).to redirect_to(authenticated_root_path)
-        expect(flash['alert']).to include 'not authorized'
-      end
-    end
-
-    context 'with an admin' do
-      let(:role) { :admin }
+    context 'with an authorized user' do
+      before { sign_in create(:user, :admin) }
 
       it 'shows the system actions index page' do
         get system_actions_path
         expect(response).to have_http_status :ok
+      end
+    end
+  end
+
+  # POST /system_actions/refresh_all_ils_metadata
+  context 'when refreshing all ILS metadata' do
+    before { sign_in create(:user, role) }
+
+    context 'with an authorized user' do
+      let(:role) { :admin }
+
+      before do
+        post refresh_all_ils_metadata_system_actions_path
+      end
+
+      it 'displays a job enqueued alert' do
+        follow_redirect!
+        expect(response.body).to include I18n.t('actions.item.refresh_all_ILS.success')
+      end
+
+      it 'enqueues the job' do
+        expect(EnqueueBulkRefreshIlsMetadataJob).to have_enqueued_sidekiq_job.with any_args
+      end
+    end
+
+    context 'when an error occurs while enqueueing the job' do
+      let(:role) { :admin }
+
+      before do
+        allow(EnqueueBulkRefreshIlsMetadataJob).to receive(:perform_async).and_return(nil)
+
+        post refresh_all_ils_metadata_system_actions_path, params: { form: 'refresh_all_ILS_metadata' }
+      end
+
+      it 'displays failure alert' do
+        follow_redirect!
+        expect(response.body).to include I18n.t('actions.item.refresh_all_ILS.failure')
+      end
+    end
+
+    context 'with an unauthorized user' do
+      let(:role) { :editor }
+
+      before { post refresh_all_ils_metadata_system_actions_path }
+
+      it 'displays failure alert' do
+        expect(response).to redirect_to(authenticated_root_path)
+        expect(flash['alert']).to include 'not authorized'
       end
     end
   end
