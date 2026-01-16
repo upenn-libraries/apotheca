087abccfa67cf8f0539ff5d2af0f28b7ac8134e5
Amrey Mathurin <mamrey@upenn.edu>
2026-01-16 17:39:17 -0500

    Convert to alma specs

--- rails_app/spec/services/metadata_extractor/alma_spec.rb
+++ rails_app/spec/services/metadata_extractor/alma_spec.rb
@@ -1,27 +1,25 @@
 # frozen_string_literal: true
 
-describe MetadataExtractor::Marmite do
-  let(:url) { Settings.marmite.url }
-
+describe MetadataExtractor::Alma do
   describe '.new' do
-    subject(:marmite) { described_class.new(url: url) }
+    subject(:alma) { described_class.new }
 
     it { is_expected.to be_a described_class }
 
     it 'creates client' do
-      expect(marmite.client).to be_a MetadataExtractor::Marmite::Client
+      expect(alma.client).to be_a MetadataExtractor::Alma::Client
     end
   end
 
   describe '#descriptive_metadata' do
-    subject(:metadata) { marmite.descriptive_metadata(bibnumber) }
+    subject(:metadata) { alma.descriptive_metadata(bibnumber) }
 
-    let(:marmite) { described_class.new(url: url) }
+    let(:alma) { described_class.new }
     let(:bibnumber) { MMSIDValidator::EXAMPLE_VALID_MMS_ID }
 
     context 'when record is found' do
-      include_context 'with successful Marmite request' do
-        let(:xml) { File.read(file_fixture('marmite/marc_xml/book-1.xml')) }
+      include_context 'with successful Alma request' do
+        let(:xml) { File.read(file_fixture('alma/marc_xml/book-1.xml')) }
       end
 
       it 'returns descriptive metadata' do
